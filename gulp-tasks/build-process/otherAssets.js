ee1d78970c2a2674e7131eb7e2a84b549b32c0a5
AsifAmin <asif_udr@yahoo.com>
2016-07-11 11:26:14 +1000

    code formatting

--- gulp-tasks/build-process/otherAssets.js
+++ gulp-tasks/build-process/otherAssets.js
@@ -1,9 +1,7 @@
 module.exports = function (gulp, plugins, config) {
     return function () {
         gulp.src(config.src.images()+'*').pipe(gulp.dest(config.build.images()));
-
         gulp.src(config.src.lib()+'*'+'*').pipe(gulp.dest(config.build.lib()));
-
         gulp.src(config.src.includes()+'*'+'*').pipe(gulp.dest(config.build.includes()));
     };
 };
