f836097fce24ed54cdc802dc2744cc4bf62be0a0
AsifAmin <asif_udr@yahoo.com>
2016-08-01 13:46:16 +1000

    secondary active nav feature addition and other js changes

--- src/assets/js/sections/nav/current-secondary-nav.js
+++ src/assets/js/sections/nav/current-secondary-nav.js
@@ -1,50 +1,39 @@
 "use strict";
-var activeSideNav = {
-    init : function () {
-        const currentFilename = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1);
-        this.highlightNavItem();
-    },
-    getCurrentTitle : function  () {
+var activeSideNav = (function () {
+    const currentFilename = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1);
+
+    function refineText(text) {
+        return text.toLowerCase().replace(/ /g,'');
+    }
+
+    function  getCurrentTitle() {
         var currentPageTitle = '';
         if ( $('#guide-title').length > 0 ) {
             currentPageTitle = $('#guide-title').text();
         }
-        else if ($('meta[name="DCTERMS.alternative"]').length > 0 && $('meta[name="DCTERMS.alternative"]').eq(0).attr('content') !== '') {
-             currentPageTitle = $('meta[name="DCTERMS.alternative"]').eq(0).attr('content');
+        else if ($('meta[name="DCTERMS.alternative"]').length > 0 && refineText($('meta[name="DCTERMS.alternative"]').eq(0).attr('content')) !== '') {
+            currentPageTitle = $('meta[name="DCTERMS.alternative"]').eq(0).attr('content');
         }
         else {
             let titleClone = $('h1', '#qg-primary-content').eq(0).clone();
             titleClone.find('.page-number').remove();
             currentPageTitle = titleClone.text();
         }
-        return currentPageTitle;
-    },
-    highlightNavItem : function () {
-        var currentPageTitle = this.getCurrentTitle();
+        return refineText(currentPageTitle);
+    }
+
+    function highlightNavItem() {
+        var currentPageTitle = getCurrentTitle();
         $(".qg-section-nav ul>li").each(function (index) {
-            if($.trim($(this).text()) === $.trim(currentPageTitle)){
+            if(refineText($(this).text()) === $.trim(currentPageTitle)){
                 $(this).find("a").addClass("active");
             }
         });
     }
-};
+
+    return {
+        highlightNavItem : highlightNavItem
+    }
+})();
 
 module.exports = activeSideNav;
-
-// Highlight current area in TOC
-// if (document.getElementById('toc') !== null) {
-//     // mark current page in guide
-//     $('a[href="' + currentFilename + '"]', '#toc').parent().addClass('current');
-//     $('.link-text', '#toc').filter(function () {
-//         return $.trim($(this).text()) === $('h2', '#content').eq(0).text();
-//     }).closest('li').addClass('current');
-//
-//     // Embed progress menu for pages that contain it (don't do this for the business franchise)
-//     $('.current-page', '#nav-section')
-//         .addClass('has-submenu')
-//         .append($('#toc ol').clone());
-// }
-// // Highlight current area in progress bar
-// if ($('#guide-progress').length > 0) {
-//     $('a[href="' + currentFilename + '"]', '#guide-progress').parent().addClass('current');
-// }
--- src/assets/js/sections/nav/mobile-nav.js
+++ src/assets/js/sections/nav/mobile-nav.js
@@ -1,31 +1,37 @@
-var nav = function () {
-    $("#qg-show-menu").on("click" , function () {
-        $("#qg-breadcrumb>.qg-inner , #qg-site-nav").slideToggle("fast");
-    });
-    $("#qg-show-search").on("click" , function () {
-        $(".qg-tools").slideToggle("fast");
-    });
+"use strict";
+var mobileNav = (function () {
+    var $sitenav = $("#qg-site-nav");
+    var $br = $("#qg-breadcrumb>.qg-inner");
+    var $tools = $(".qg-tools");
 
-    $(window).resize(function () {
-        var $sitenav = $("#qg-site-nav");
-        var $br = $("#qg-breadcrumb>.qg-inner");
-        var $tools = $(".qg-tools");
-        if ($(window).width() < 768) {}
-        else if ($(window).width() >= 768 &&  $(window).width() <= 992) {
-            if($tools.is(":visible")){
-                $tools.hide("fast");
+    function interactions() {
+        $("#qg-show-menu").on("click" , function () {
+            $("#qg-breadcrumb>.qg-inner , #qg-site-nav").slideToggle("fast");
+        });
+        $("#qg-show-search").on("click" , function () {
+            $(".qg-tools").slideToggle("fast");
+        });
+        $(window).resize(function () {
+            if ($(window).width() < 768) {}
+            else if ($(window).width() >= 768 &&  $(window).width() <= 992) {
+                if($tools.is(":visible")){
+                    $tools.hide("fast");
+                }
             }
-        }
-        else  {
-            if($sitenav.is(":hidden")){
-                $sitenav.slideToggle("fast");
-                $br.slideToggle("fast");
+            else  {
+                if($sitenav.is(":hidden")){
+                    $sitenav.slideToggle("fast");
+                    $br.slideToggle("fast");
+                }
+                if($tools.is(":hidden")){
+                    $tools.slideToggle("fast");
+                }
             }
-            if($tools.is(":hidden")){
-                $tools.slideToggle("fast");
-            }
-        }
-    });
-};
+        });
+    }
+    return {
+        interactions : interactions
+    }
+})();
 
-module.exports = nav;
+module.exports = mobileNav;
