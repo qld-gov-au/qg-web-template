18794ed39ef749cd2ce5d2edf6f888d3d4b1f03a
AsifAmin <asif_udr@yahoo.com>
2016-08-11 13:26:00 +1000

    build system updates

--- gulp-tasks/build-process/content.js
+++ gulp-tasks/build-process/content.js
@@ -1,11 +1,10 @@
 module.exports = function (gulp, plugins, config) {
     return function () {
-        var tasks = config.projects.map(function (element, index, array) {
+        config.projects.map(function (element, index, array) {
             return gulp.src([config.basepath.src+element+'/**/*',
                 '!' + config.basepath.src + '{'+element+'/assets,'+element+'/assets/**}'
             ], {dot: true})
                 .pipe(gulp.dest(config.basepath.build + '/'+element+'/'));
         });
-        return tasks;
     };
 };
