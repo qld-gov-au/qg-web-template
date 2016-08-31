952f0b49755baf70de46778500ff48762ae17c76
AsifAmin <asif_udr@yahoo.com>
2016-08-31 15:00:11 +1000

    show hide general functionality

---
+++ src/assets/js/components/utils.js
@@ -0,0 +1,28 @@
+var utils = (function () {
+    function showHide() {
+        var $button = $('.qg-toggle-btn');
+        var $content = $('.qg-toggle-content');
+
+        $button.each(function() {
+            $(this).click(function (e) {
+                e.preventDefault();
+                var $content = $(this).parents('.row').last().next($content);
+                if ($content.hasClass("qg-visually-hidden")) {
+                    $content.slideUp(0, function() {
+                        $content.removeClass('qg-visually-hidden').slideDown('fast');
+                    });
+                } else {
+                    $content.slideUp('fast', function() {
+                        $content.addClass('qg-visually-hidden').slideDown(0);
+                    });
+                }
+            });
+        });
+    }
+
+    return {
+        showHide : showHide
+    }
+})();
+
+export default utils;
