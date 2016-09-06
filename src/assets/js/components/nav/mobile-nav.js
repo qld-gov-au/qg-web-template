4853063f897a358d20bfacd8eb7ca2e93da62f84
eru <epenkman@thoughtworks.com>
2016-09-06 11:54:22 +1000

    [EP] support defferred loading of the mobile nav by using jquery event delegation and running jquery queries after the elements are loaded

--- src/assets/js/components/nav/mobile-nav.js
+++ src/assets/js/components/nav/mobile-nav.js
@@ -4,38 +4,38 @@ import utils from '../global/utils.js';
 
 // TODO - remove specific show hide function and make it use the general show hide in utils
 var mobileNav = (function () {
-    var $sitenav = $('#qg-site-nav'),
-        $br = $('#qg-breadcrumb'),
-        $tools = $('.qg-tools');
+  var siteNav = '#qg-site-nav',
+    br = '#qg-breadcrumb',
+    tools = '.qg-tools';
 
-    function interactions() {
-        $('#qg-show-menu').on('click', function () {
-            $br.add($sitenav).slideToggle(200);
-        });
-        $('#qg-show-search').on('click', function () {
-            $('#qg-search-form').toggleClass('qg-visually-hidden-md', 1500);
-        });
-        $(window).resize(function () {
-            if ($(window).width() < utils.breakpoints.bsSm) {
-                // any code
-            } else if ($(window).width() >= utils.breakpoints.bsSm && $(window).width() <= utils.breakpoints.bsMd) {
-                if ($tools.is(':visible')) {
-                    $tools.hide(1);
-                }
-            } else {
-                if ($sitenav.is(':hidden')) {
-                    $sitenav.slideToggle(1);
-                    $br.slideToggle(1);
-                }
-                if ($tools.is(':hidden')) {
-                    $tools.slideToggle(1);
-                }
-            }
-        });
-    }
-    return {
-        interactions: interactions
-    };
+  function interactions () {
+    $(document).on('click', '#qg-show-menu', function () {
+      $(br).add($(siteNav)).slideToggle(200);
+    });
+    $(document).on('click', '#qg-show-search', function () {
+      $('#qg-search-form').toggleClass('qg-visually-hidden-md', 1500);
+    });
+    $(window).resize(function () {
+      if ($(window).width() < utils.breakpoints.bsSm) {
+        // any code
+      } else if ($(window).width() >= utils.breakpoints.bsSm && $(window).width() <= utils.breakpoints.bsMd) {
+        if ($(tools).is(':visible')) {
+          $(tools).hide(1);
+        }
+      } else {
+        if ($(siteNav).is(':hidden')) {
+          $(siteNav).slideToggle(1);
+          $(br).slideToggle(1);
+        }
+        if ($(tools).is(':hidden')) {
+          $(tools).slideToggle(1);
+        }
+      }
+    });
+  }
+  return {
+    interactions: interactions
+  };
 })();
 
 module.exports = mobileNav;
