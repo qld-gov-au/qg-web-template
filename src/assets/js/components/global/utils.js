af6d030b3f7c44ef65ec2fee39df5cf3cc639ffa
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2016-09-22 15:53:31 +1000

    Disabled bootstrap accessibility plugin due to errors.
    Did some other cleanup too.

--- src/assets/js/components/global/utils.js
+++ src/assets/js/components/global/utils.js
@@ -1,21 +1,8 @@
-// Bootstrap / Jquery extension for collapse
-
-(function($) {
-  "use strict";
-
-    // this[this.$element.hasClass('in') ? 'hide' : 'show']()
-    var collToggle = $.fn.collapse.Constructor.prototype.toggle;
-    $.fn.collapse.Constructor.prototype.toggle = function(){
-        if( this.$trigger.is("input[type=radio]") ) {
-            if( this.$trigger.prop("checked", true) && ! this.$element.attr('aria-expanded') != true ) {
-                this.show();
-            }
-        } else {
-            collToggle.call(this); // Default behaviour
-        }
-    }
-})(jQuery);
-
+/* ========================================================================
+* General QLD GLUE utilities
+* [TODO: Write about what this is for, to make it easier for future
+* developers to know what to put into it, and what not to.]
+* ======================================================================== */
 
 const utils = (function () {
     // BREAKPOINTS //
@@ -26,134 +13,7 @@ const utils = (function () {
         bsLg: 1200
     };
 
-    // SHOW / HIDE TOGGLE DISPLAY //
-    // THIS IS ONLY KEPT HERE FOR HISTORICAL REASONS, YOU MAY DELETE
-    // This function interacts with CSS to define 'hidden'
-    /*
-    To initialise this on a page, add the attribute 'data-qg-showhide-target' to your trigger element (usually a button).
-    Give this attribute a CSS selector for the element you wish to show / hide
-    Options:
-    - data-qg-parent    : Set a parent element if you wish to toggle other elements off
-    - data-qg-show      : Force the element to allways be displayed at a certain size. Set to 'sm', 'md' or 'lg'
-    - data-qg-aria-safe : Set true/false depending on if you want ARIA to be able to read the element when hidden (default: true)
-    - data-qg-default   : Set to 'show' or 'hide' to set the default behaviour of the target element (default: hide)
-    */
-    /*
-    var attrTarget      = 'data-qg-showhide-target',
-        attrParent      = 'data-qg-parent',
-        attrShow        = 'data-qg-show',
-        attrAriaSafe    = 'data-qg-aria-safe',
-        attrDefault     = 'data-qg-default',
-        $trigger        = $('*[' + attrTarget + ']');
-
-    jQuery.fn.select = function( target ){
-        var $target;
-        if( $( target ).length ) {
-            $target = $( target );
-        } else if( $( target, this ).length ){
-            $target = $( target, this );
-        }
-        return $target;
-    };
-
-    var showHideAction = function( thisTrigger, state, duration ){
-
-        state = ( state == undefined )? false: state; // To force the state of the action
-        duration = ( duration == undefined )? 'fast': duration;
-
-        var $thisTrigger = $(thisTrigger),
-            $target = $thisTrigger.select( $thisTrigger.attr(attrTarget) ),
-            $parent = false,
-            show = 'auto', // Default value
-            ariaSafe = true, // Default true
-            ariaHiddenClass = 'qg-aria-hidden';
-
-        if( typeof $thisTrigger.attr( attrParent ) !== undefined ) {
-            $parent = $( $thisTrigger.attr( attrParent ) );
-        }
-        if( typeof $thisTrigger.attr( attrShow ) !== undefined ) {
-            show = $thisTrigger.attr( attrShow );
-        }
-        if( typeof $thisTrigger.attr( attrAriaSafe ) !== undefined ) {
-            ariaSafe = $thisTrigger.attr( attrAriaSafe );
-        }
-        switch( show ) {
-            case "sm":
-                ariaHiddenClass = 'qg-aria-hidden-show-sm';
-                break;
-            case "md":
-                ariaHiddenClass = 'qg-aria-hidden-show-md';
-                break;
-            case "lg":
-                ariaHiddenClass = 'qg-aria-hidden-show-lg';
-                break;
-        }
-
-        if( state === false ){
-            state = ( $thisTrigger.hasClass('active') )? 'hide': 'show';
-        }
-
-        if( state === 'show' ) {
-            var tempClass = 'qg-temp-active-element';
-            $thisTrigger.addClass( tempClass );
-            if( $parent ){
-                $parent.find('*['+attrTarget+']').each(function(){
-                    if( ! $(this).hasClass( tempClass ) ){
-                        showHideAction( this, 'hide' ); // hide all other elements
-                    }
-                });
-            }
-            $thisTrigger.removeClass( tempClass );
-
-            $thisTrigger.addClass('active');
-            if( ariaSafe ) {
-                $target.slideUp(0,function(){
-                    $target.removeClass(ariaHiddenClass)
-                    .slideDown(duration);
-                });
-            } else {
-                $target.slideDown(duration);
-            }
-        } else if( state === 'hide' ) {
-            $thisTrigger.removeClass('active');
-            if( ariaSafe ) {
-                $target.slideUp(duration,function(){
-                    $target.addClass(ariaHiddenClass)
-                    .slideDown(0);
-                });
-            } else {
-                $target.slideUp(duration);
-            }
-        } else {
-            // console.error('An impossible error occured in utils.js');
-        }
-    }
-    var showHide = function () {
-
-        $trigger.each( function(){
-            var hide = 'hide';
-            if( typeof $(this).attr( attrDefault ) !== undefined ) {
-                hide = $(this).attr( attrDefault );
-            }
-            showHideAction( this, hide, 0 );
-
-            if( $(this).is("input[type=radio]") ) {
-                $(this).on('change', function(){
-                    if( $(this).prop("checked", true) ){
-                        showHideAction( this, 'show' );
-                    }
-                });
-            } else {
-                $(this).click(function(){
-                    showHideAction( this );
-                });
-            }
-        });
-    };
-    */
-
     return {
-        // showHide : showHide,
         breakpoints: breakpoints
     };
 })();
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,19 @@
+/*
+* Imports Javascript components for the GLUE
+*/
+
 import '../../../node_modules/bootstrap/dist/js/bootstrap.js';
-import './components/global/bootstrap-accessibility.js';
+
+// TODO: Find a new bootstrap accessibility plugin, build one, or figure out how to get  this one working properly
+// Disabled because it creates accessibility errors (strangely enough)
+// import './components/global/bootstrap-accessibility.js';
 
 import './components/autocomplete.js';
 import './components/forms.js';
 // import mobileNav from './components/nav/mobile-nav.js'
 import activeSideNav from './components/nav/current-secondary-nav';
 import utils from './components/global/utils.js';
+import qgBsExtend from './components/global/bootstrap-extensions.js';
 import feedbackForm from './components/feedback-form';
 
 (function () {
@@ -14,6 +22,4 @@ import feedbackForm from './components/feedback-form';
   activeSideNav.highlightNavItem();
   var franchiseTitle = qg && qg.swe && qg.swe.franchiseTitle;
   feedbackForm.init(franchiseTitle);
-
-// utils.showHide()
 }());
