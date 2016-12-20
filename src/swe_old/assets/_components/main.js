cd6de15b000df95480f7bd5d7ef565ac54fcd26c
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2016-12-20 13:16:08 +1000

    Folder cleanup final.

--- src/swe_old/assets/_components/main.js
+++
@@ -1,7 +0,0 @@
-// Just imports core.js
-var src = {
-		core: '../../../core/assets/_components/',
-		node_modules: '../../../../node_modules/'
-	};
-
-import '../../../core/assets/_components/main.js'; // Inherit core main.js
