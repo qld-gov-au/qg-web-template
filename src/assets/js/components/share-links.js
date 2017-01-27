80e9eda87bc17edc03d770fdabc616454915ad88
AsifAmin <asif_udr@yahoo.com>
2017-01-27 16:10:36 +1000

    changed url param name

--- src/assets/js/components/share-links.js
+++ src/assets/js/components/share-links.js
@@ -1,10 +1,10 @@
 function init () {
-    var currUrl = window.location.href;
+    var pageUrl = window.location.href;
     var links = $('.qg-share-link');
     var pageTitle = $(document).find('title').text();
     links.each(function (index, link) {
         link.href = link.href.replace('PAGE_TITLE', pageTitle);
-        link.href = link.href.replace('PAGE_URL', encodeURIComponent(currUrl));
+        link.href = link.href.replace('PAGE_URL', encodeURIComponent(pageUrl));
     });
 }
 module.exports = {
