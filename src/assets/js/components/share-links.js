3e5add67e62624327be7a985e13945cc18ab1d35
AsifAmin <asif_udr@yahoo.com>
2017-01-27 15:08:04 +1000

    added url for social sharing function

--- src/assets/js/components/share-links.js
+++ src/assets/js/components/share-links.js
@@ -1,8 +1,10 @@
 function init () {
+    var currUrl = window.location.href;
     var links = $('.qg-share-link');
     var pageTitle = $(document).find('title').text();
     links.each(function (index, link) {
         link.href = link.href.replace('PAGE_TITLE', pageTitle);
+        link.href = link.href.replace('CURR_URL', encodeURIComponent(currUrl));
     });
 }
 module.exports = {
