8f88c4ecdcd5415b1ce107f604dea5f965fce5b7
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2017-03-07 09:48:38 +1000

    Re-arranging bootstrap button mixin loader

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,6 @@ var gulp        = require('gulp'),
     // include     = require('gulp-include'),
     // replace     = require('gulp-replace');
 
-var projectName = 'DEFAULT'; // Used for the build process
-
-
-
 /* BUILD TASKS */
 gulp.task('scss', require('./gulp/build-tasks/scss')(gulp, plugins, config));
 gulp.task('html', require('./gulp/build-tasks/html')(gulp, plugins, config));
@@ -30,8 +26,8 @@ gulp.task('build', ['default']);
 /* WATCH TASSKS */
 gulp.task('watch', function () {
     // gulp.watch(config.basepath.src + '/**/*.js', ['js']);
-    gulp.watch([config.basepath.src + '/**/*.scss'], ['scss']);
     gulp.watch([config.basepath.src + '/**/*.html'], ['html']);
+    gulp.watch([config.basepath.src + '/**/*.scss'], ['scss']);
     // gulp.watch([config.basepath.src + '*', config.basepath.src + '*' + '*', config.basepath.src + '*' + '*'], ['other:assets']);
 });
 
