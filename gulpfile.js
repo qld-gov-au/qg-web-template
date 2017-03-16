4b997a088f97f666f341976e8fdafe1963865b24
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2017-03-16 10:32:13 +1000

    Updated gulp test tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp        = require('gulp'),
     del         = require('del'),
     webpack     = require('webpack'),
     argv        = require('yargs').argv,
-    plugins     = require('gulp-load-plugins')();
+    plugins     = require('gulp-load-plugins')(),
+    karmaServer = require('karma').Server;
     // bowerConfig = require('./bower.json'),
     // gulpConnect = require('gulp-connect'),
     // runSequence = require('run-sequence'),
@@ -34,3 +35,10 @@ gulp.task('watch', function () {
 /* RELEASE TASKS */
 gulp.task('scss-src', require('./gulp/release-tasks/scss-src')(gulp, plugins, config));
 gulp.task('release', ['scss-src']);
+
+/* TEST TASKS */
+gulp.task('test:unit', require('./gulp/test-tasks/unit')(gulp, plugins, config, karmaServer));
+gulp.task('test:eslint', require('./gulp/test-tasks/lint')(gulp, plugins, config, fsPath, eslintReporter));
+gulp.task('test:config:e2e', require('./gulp/test-tasks/e2e')(gulp, plugins, config));
+gulp.task('test:browserstack', ['local-server', 'test:config:e2e']);
+gulp.task('test:reports', require('./gulp/test-tasks/reports')(gulp, plugins, config));
