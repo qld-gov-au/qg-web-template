7f5b755f212d3f010ef6cbc1cbd0a58b37aeefeb
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2017-03-21 16:58:23 +1000

    Updated documentation, and header styles.

--- src/template/assets/_project/js/modules/accessibility.js
+++ src/template/assets/_project/js/modules/accessibility.js
@@ -11,9 +11,8 @@ var accessibility = (function ($) {
         var $target = $('*[target=_blank]');
         if(!$target.hasClass('qg-accessibility-off')) {
             if($target.attr('href') !== undefined) {
-                if($.contains('.sr-only', $target) === false) {
-                    console.log('3');
-                    $target.append(' <span class="sr-only">(Opens in new window)</span> ');
+                if($.contains('.qg-blank-notice', $target) === false) {
+                    $target.append(' <span class="qg-blank-notice sr-only">(Opens in new window)</span> ');
                 }
                 if($target.attr('title') === undefined) {
                     $target.attr('title', 'Opens in new window');
