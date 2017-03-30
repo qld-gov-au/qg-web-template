5a5155dbb0dc738a0704045fac20214e7aa4f022
Nimrod Evans <nimrod.evans@dsiti.qld.gov.au>
2017-03-31 09:39:30 +1000

    Updating linting

--- gulp/build-tasks/includes.js
+++ gulp/build-tasks/includes.js
@@ -7,8 +7,11 @@ module.exports = function (gulp, plugins, config) {
         `${config.basepath.src}/assets/includes/**/*.html`,
       ].concat(config.build.excludes);
 
+      let projectAssets = new RegExp('="/assets/_project/', 'g');
       return gulp.src(target, { dot: true })
         .pipe(plugins.include({ hardFail: true }))
+        // Replace /assets/_project/ with /assets/v3/
+        .pipe(plugins.replace(projectAssets, `="/assets/${config.versionName}/`))
         .on('error', console.log)
         .pipe(gulp.dest(`${config.basepath.build}/assets/includes/`))
         .on('end', cb);
