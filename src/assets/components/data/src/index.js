be8bc862ce6cbb8a897f7d71de50f3951079f9b0
gvanimurthy <gvanimurthy@gmail.com>
2017-05-25 11:14:02 +1000

    Feature/qol 1101 maps js transfer (#58)
    * component level build system
    
    * components workflow updated
    
    * dependencies in components
    
    * QOL-1103 Added dependency pagination component
    
    * edits pagination
    
    * QOL-1104-Data Module Transfer

---
+++ src/assets/components/data/src/index.js
@@ -0,0 +1,24 @@
+/*global qg*/
+
+//plugin
+import './lib/jquery.jsonp';
+import './lib/data';
+
+// init code
+$(function () {
+	var dataUrl = $('#data-url').data('url'),
+		dataEnvironment = dataUrl.indexOf('staging.data.qld.gov.au') >= 0 ? 'staging.data.qld.gov.au' : 'data.qld.gov.au',
+		resourceId = dataUrl.split('resource/')[1],
+		sql = 'Select * from ' + resourceId;
+	qg.data.get(dataEnvironment, sql, {
+		cache: !0,
+		successCallback: function (data) {
+		// var titleField = displayTemplateHtml.replace( /^[\s\S]*?\{\{.*?\}\}.*?\{\{(.*?)\}\}[\s\S]*$/m, '$1' );
+			if (data.result.records.length > 0) {
+				$('.search-result').html(data.result.records);
+			} else {
+				$('.search-result').html('No records found');
+			}
+		}
+	});
+});
