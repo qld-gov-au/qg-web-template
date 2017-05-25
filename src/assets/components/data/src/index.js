0a96282688d578a41ad115e4991ac1a2fa60e8bf
Vani Ganapa <gvanimurthy@gmail.com>
2017-05-25 13:11:29 +1000

    QOL-1104-Data module without error handling

--- src/assets/components/data/src/index.js
+++ src/assets/components/data/src/index.js
@@ -9,13 +9,15 @@ $(function () {
 	var dataUrl = $('#data-url').data('url'),
 		dataEnvironment = dataUrl.indexOf('staging.data.qld.gov.au') >= 0 ? 'staging.data.qld.gov.au' : 'data.qld.gov.au',
 		resourceId = dataUrl.split('resource/')[1],
-		sql = 'Select * from ' + resourceId;
+		sql = 'Select * from "' + resourceId + '"';
 	qg.data.get(dataEnvironment, sql, {
 		cache: !0,
 		successCallback: function (data) {
 		// var titleField = displayTemplateHtml.replace( /^[\s\S]*?\{\{.*?\}\}.*?\{\{(.*?)\}\}[\s\S]*$/m, '$1' );
 			if (data.result.records.length > 0) {
-				$('.search-result').html(data.result.records);
+				$.each(data.result.records, function (k, v) {
+					$('.search-result').append(v.Title + '<br>');
+				});
 			} else {
 				$('.search-result').html('No records found');
 			}
