7046a73e56cb17b015c112a2543a89b430a44bd4
Vani Ganapa <gvanimurthy@gmail.com>
2017-05-31 08:07:34 +1000

    QOL-1348, QOL-1380. Moving dependencies

---
+++ src/assets/components/jquery.resize-events/webpack.config.babel.js
@@ -0,0 +1,40 @@
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: resolve('src'),
+  entry: './index.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'jquery.resize-events.js',
+    publicPath: '/build/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+      },
+      {
+        test: /\.css$/,
+        loaders: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+      },
+
+    ],
+    rules: [
+      { enforce: 'pre',test: /\.js$/, exclude: /node_modules/, loader: "eslint-loader" },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin(`styles/jquery.resize-events.css`),
+    new HtmlWebpackPlugin({
+      title: 'build template',
+      inject: false,
+      template: './examples/index.html'
+    }),
+  ],
+};
