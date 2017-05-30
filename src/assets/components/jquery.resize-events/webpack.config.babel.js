a5f94c96231e95db6ac139de64b4074b5b8cc3a8
Vani Ganapa <gvanimurthy@gmail.com>
2017-05-31 09:58:48 +1000

    QOL-1380-Resize Events

--- src/assets/components/jquery.resize-events/webpack.config.babel.js
+++ src/assets/components/jquery.resize-events/webpack.config.babel.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+        test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'],
       },
       {
         test: /\.scss$/,
