6a93e3adbd16ab8034f260620864cc44187e7956
AsifAmin <asif_udr@yahoo.com>
2017-06-08 13:48:53 +1000

    updated demo location

--- gulp/build-tasks/template-pages.js
+++ gulp/build-tasks/template-pages.js
@@ -17,8 +17,6 @@ module.exports = function (gulp, plugins, config, src, dest, type = 'cdn') {
       .pipe(plugins.replace(projectAssets, `="$1assets/${config.versionName}/`))
       .pipe(plugins.if(type === 'local', plugins.replace(cdnIncludes, `="$1assets/includes-local/`))) // Replace cdn assets with local assets
       .on('error', console.log)
-      // to add atom file types
-      .pipe(plugins.addSrc(`${config.basepath.src}/${src}/**/*.atom`))
       .pipe(gulp.dest(`${config.basepath.build}/${dest}/`));
   };
 };
