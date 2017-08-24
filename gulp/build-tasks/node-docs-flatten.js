5fded0fa723aebce33b36b94a8b7b2de07eb700d
ganapavz <30642901+ganapavz@users.noreply.github.com>
2017-08-24 12:48:22 +1000

    QOL-1773-created templates without SSIs (#90)

--- gulp/build-tasks/node-docs-flatten.js
+++ gulp/build-tasks/node-docs-flatten.js
@@ -4,8 +4,6 @@
 
 const path = require('path');
 const ssiToStatic = require('./ssi-to-static.js');
-// TODO: Delete /includes-local/ after flatten process
-// const del = require('del');
 
 let includeSrc = path.join('build', 'docs');
 let src = path.join('build', 'docs');
@@ -14,4 +12,3 @@ let exclude = [];
 
 ssiToStatic(includeSrc, src, dest, exclude);
 
-// del('build/docs/includes-local/');
