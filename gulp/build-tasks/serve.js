db02b90735716daea505232328e1fa35b18aab11
AsifAmin <asif_udr@yahoo.com>
2017-08-25 14:51:45 +1000

    [QOL-2150]-cleaned gulp serve
    # Conflicts:
    #	gulp/build-tasks/modules.js

--- gulp/build-tasks/serve.js
+++ gulp/build-tasks/serve.js
@@ -1,22 +1,15 @@
 module.exports = function (gulp, plugins, connect, connectssi, argv, path) {
   'use strict';
   return () => {
-    let root = [path.join(__dirname, '..', '..', 'build')];
-    let port = (argv.port === undefined) ? 7777 : argv.port;
-    if (argv.type === 'reports') {
-      port = 9999;
-      root = [path.join(__dirname, '..', '..', 'tests', 'reports')];
-    } else if (argv.type === 'release') {
-      port = 8888;
-      root = [path.join(__dirname, '..', '..', 'release')];
-    }
+    let root = argv.root ? path.resolve(argv.root) : path.resolve('build');
+    let port = 7777;
     connect.server({
       root: root,
       port: port,
       livereload: true,
       middleware: function () {
         return [connectssi({
-          baseDir: path.join(__dirname, '..', '..', 'build'),
+          baseDir: root,
           ext: '.html',
           onlineEncoding: 'utf8',
           localEncoding: 'utf8',
