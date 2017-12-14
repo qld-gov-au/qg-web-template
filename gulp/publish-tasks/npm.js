ecd6e30778c69c1e31956d03c25bcabf8d77bc7b
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2017-12-15 09:55:42 +1000

    Qol 2555 npm publishing script updates (#104)
    * [QOL-2555] - NPM publishing script updatese
    
    * [QOL-2555] - NPM publishing script updates

--- gulp/publish-tasks/npm.js
+++ gulp/publish-tasks/npm.js
@@ -1,18 +1,22 @@
 'use strict';
+const path        = require('path');
 
 module.exports = function (gulp, plugins, config, argv) {
   return function () {
     let releaseTypes = ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease'];
     return gulp.src(`${config.basepath.release}/**/*`, { dot: true })
-      .pipe(gulp.dest(`${config.publish.npmDir}`))
       .pipe(plugins.prompt.prompt([{
+         type: 'input',
+         name: 'confirmation',
+         message: 'Please confirm that you have cloned npm package Github repo(https://github.com/qld-gov-au/web-template-release.git) as a sibling dir to qg-web-template? (y or n)',
+        }, {
         type: 'input',
         name: 'confirmation',
         message: 'Please confirm that you want to publish this package on NPM? (y or n)',
       }, {
         type: 'input',
         name: 'logged',
-        message: 'Are you logged into the NPM as the correct user? (y or n)',
+        message: 'Are you logged into the NPM as the correct user (Run \'npm whoami\' to check you logged in as qld gov user)? (y or n)',
       }, {
         type: 'input',
         name: 'releaseType',
@@ -23,15 +27,17 @@ module.exports = function (gulp, plugins, config, argv) {
           if (/^yes|y$/.test(res.confirmation.toLowerCase()) && /^yes|y$/.test(res.logged.toLowerCase()) && checkInput.length > 0) {
             plugins.shell.task([
               'echo release type "' + res.releaseType + '"',
-              process.chdir(config.publish.npmDir),
-              'pwd',
-              'git remote set-url origin https://github.com/qld-gov-au/web-template-release',
+              process.chdir(path.join('..', 'web-template-release')),
+              'sudo cp -a ../qg-web-template/release/. ../web-template-release/',
+              'ls',
+              'git checkout master',
+              'git clean --force -d -q',
+              'git pull origin master',
               'git add --all',
               'git commit -m "' + argv.msg + '"',
               'npm version ' + res.releaseType,
-              'git pull -X ours',
               'git push origin master --tags',
-              'npm publish',
+              'npm publish'
             ])();
           } else {
             console.log('Please make sure you are logged into the NPM and you have selected a release type');
--- readme/deploy.md
+++ readme/deploy.md
@@ -1,16 +1,15 @@
-# Deploying the template
-
-## Versioning
-
-Follow semantic versioning:
-https://www.sitepoint.com/semantic-versioning-why-you-should-using/
-
-** All Major or Semi-major releases must be discussed with the product owner before release **
-
 ## How to publish release content on NPM
 
 * __npm login__
-* Clone https://github.com/qld-gov-au/web-template-release.git as a __sibling directory__ (next to the glue-template dir).
-* __gulp npm:publish --msg="your commit msg"__ to start the publishing process. 
+* Clone https://github.com/qld-gov-au/web-template-release.git as a __sibling directory__ (next to the qg-web-template dir).
+* __gulp npm:publish --msg="your commit msg"__ to start the publishing process.
   * Confirm and enter a release type.
   * Check the published content by going to the npm package home page https://www.npmjs.com/package/web-template-release.
+
+
+Useful links
+Semantic versioning explanation:
+https://www.sitepoint.com/semantic-versioning-why-you-should-using/
+
+Tutorial - Publish to npm
+https://medium.com/@jdaudier/how-to-create-and-publish-your-first-node-js-module-444e7585b738#.vsshhdfau
