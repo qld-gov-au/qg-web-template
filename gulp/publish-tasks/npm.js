5784d5d9b970b7b4efe45d6433996ddc51386f86
ganapavz <vani.ganapa@dsiti.qld.gov.au>
2018-01-15 13:16:26 +1000

    QOL-3100-fixed linting errors

--- gulp/publish-tasks/npm.js
+++ gulp/publish-tasks/npm.js
@@ -2,43 +2,42 @@
 
 const gulp = require('gulp');
 const npm = require('npm');
-const del = require('del');
 const prompt = require('gulp-prompt');
 const path = require('path');
 const fs = require('fs');
 const config = require('../gulp-config');
-const pjson = require(path.resolve(config.webTemplateRepo.folder, 'package.json'));
-
-const uri = 'http://registry.npmjs.org/',
-  username = 'qld-gov-au-npm',
-  email = 'gvanimurthy@gmail.com',
-  pkg_id = pjson.name + '-' + pjson.version + '.tgz';
-
-let auth;
 
 module.exports = (cb) => {
+  const pjson = require(path.resolve(config.webTemplateRepo.folder, 'package.json'));
+
+  const uri = 'http://registry.npmjs.org/';
+  const username = 'qld-gov-au-npm';
+  const email = 'gvanimurthy@gmail.com';
+  const pkgId = pjson.name + '-' + pjson.version + '.tgz';
+
+  let auth;
   process.chdir(path.resolve(config.webTemplateRepo.folder));
-  return  npm.load(null, (err) => {
+  return npm.load(null, (err) => {
     if (err) throw err;
 
     return gulp.src('package.json')
       .pipe(prompt.prompt({
         type: 'password',
         name: 'password',
-        message: 'Please enter \'' + username + '\' npm account password'
+        message: 'Please enter \'' + username + '\' npm account password',
       }, (res) => {
         auth = {
           username: username,
           password: res.password,
           email: email,
-          alwaysAuth: true
+          alwaysAuth: true,
         };
 
         npm.registry.adduser(uri, {auth: auth}, function (err, data, raw, res) {
-          if(err) throw err;
+          if (err) throw err;
           console.log('logged in as ' + auth.username);
 
-          npm.commands.pack([], function(err){
+          npm.commands.pack([], function (err) {
             if (err) {
               throw Error(err);
             }
@@ -47,18 +46,18 @@ module.exports = (cb) => {
             const publishParams = {
               metadata: JSON.parse(JSON.stringify(pjson)),
               access: 'public',
-              body: fs.createReadStream(path.resolve(pkg_id)),
-              auth: auth
+              body: fs.createReadStream(path.resolve(pkgId)),
+              auth: auth,
             };
-            npm.registry.publish(uri, publishParams, function(err, res) {
+            npm.registry.publish(uri, publishParams, function (err, res) {
               if (err) {
                 throw Error(err);
               }
-              console.log("Publish successfull: " + JSON.stringify(res));
+              console.log('Publish successfull: ' + JSON.stringify(res));
               return true;
-            })
-          })
+            });
+          });
         });
-      }))
-  })
+      }));
+  });
 };
