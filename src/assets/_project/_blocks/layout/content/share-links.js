0161315373e1fb68cb7530f468fd2b17a3211b7b
AsifAmin <asif_udr@yahoo.com>
2018-04-16 15:54:19 +1000

    updated share function

--- src/assets/_project/_blocks/layout/content/share-links.js
+++ src/assets/_project/_blocks/layout/content/share-links.js
@@ -1,68 +1,68 @@
 /**
-* Function for rendering social media links on CUE compliant sites
-*
-* Requires:
-* - JQuery
-**/
+ * Function for rendering social media links on CUE compliant sites
+ *
+ * Requires:
+ * - JQuery
+ **/
 
 /**
-* #####################################
-* Model
-**/
+ * #####################################
+ * Model
+ **/
 
 const socialLinksList = {
   primary: [
     { title: 'Facebook', showTitle: false, icon: renderIcon('fa', 'facebook') },
-    { title: 'Twitter',  showTitle: false, icon: renderIcon('fa', 'twitter') },
+    { title: 'Twitter', showTitle: false, icon: renderIcon('fa', 'twitter') },
     { title: 'LinkedIn', showTitle: false, icon: renderIcon('fa', 'linkedin') },
+    { title: 'Others', showTitle: false, icon: renderIcon('fa', 'share-alt') },
   ],
-  secondary: [
+ /* secondary: [
     // {title: 'Delicious',    showTitle: true, icon: renderIcon('fa', 'delicious')},
-    { title: 'Digg',     showTitle: true, icon: renderIcon('fa', 'digg') },
+    { title: "Digg", showTitle: true, icon: renderIcon("fa", "digg") },
     // {title: 'Evernote',     showTitle: true, icon: renderIcon('svg', 'evernote', '/assets/v3/images/evernote-logo-white.svg')},
     // {title: 'Reddit',       showTitle: true, icon: renderIcon('fa', 'reddit')},
     // {title: 'StumbleUpon',  showTitle: true, icon: renderIcon('fa', 'stumbleupon')},
     // {title: 'Tumblr',       showTitle: true, icon: renderIcon('fa', 'tumblr')},
-    { title: 'Google+',  showTitle: true, icon: renderIcon('fa', 'google-plus') },
-  ],
+    { title: "Google+", showTitle: true, icon: renderIcon("fa", "google-plus") }
+  ]*/
 };
 
 /**
-* #####################################
-* Views
-**/
+ * #####################################
+ * Views
+ **/
 
 function renderSocialURL (who, from, title, domain, description) {
   switch (who) {
-
   case 'facebook':
     return `http://www.facebook.com/share.php?u=${from}&title=${title}`;
   case 'twitter':
     return `http://twitter.com/home?status=${title}+${from}`;
   case 'linkedin':
     return `http://www.linkedin.com/shareArticle?mini=true&url=${from}&title=${title}&source=${domain}`;
-  case 'delicious':
-    return `http://del.icio.us/post?url=${from}&title=${title}]&notes=${description}`;
-  case 'digg':
-    return `http://www.digg.com/submit?phase=2&url=${from}&title=${title}`;
-  case 'evernote':
-    return `http://www.evernote.com/clip.action?url=${from}&title=${title}`;
-  case 'reddit':
-    return `http://www.reddit.com/submit?url=${from}&title=${title}`;
-  case 'stumbleupon':
-    return `http://www.stumbleupon.com/submit?url=${from}&title=${title}`;
-  case 'tumblr':
-    return `https://www.tumblr.com/widgets/share/tool?posttype=link&content=${from}&title=${title}&caption=${description}`;
-  case 'google+':
-    return `https://plus.google.com/share?url=${from}`;
-
+  case 'others':
+    return `https://www.qld.gov.au/share?&title=${title}&url=${from}`;
+  /*  case "delicious":
+      return `http://del.icio.us/post?url=${from}&title=${title}]&notes=${description}`;
+    case "digg":
+      return `http://www.digg.com/submit?phase=2&url=${from}&title=${title}`;
+    case "evernote":
+      return `http://www.evernote.com/clip.action?url=${from}&title=${title}`;
+    case "reddit":
+      return `http://www.reddit.com/submit?url=${from}&title=${title}`;
+    case "stumbleupon":
+      return `http://www.stumbleupon.com/submit?url=${from}&title=${title}`;
+    case "tumblr":
+      return `https://www.tumblr.com/widgets/share/tool?posttype=link&content=${from}&title=${title}&caption=${description}`;
+    case "google+":
+      return `https://plus.google.com/share?url=${from}`;*/
   }
   return false;
 }
 
 function renderIcon (type, name, src = false) {
   switch (type) {
-
   case 'fa':
     return `<span class="fa fa-${name} fa-2x qg-share-icon" aria-hidden="true"></span>`;
   case 'svg':
@@ -78,29 +78,21 @@ function renderHidden () {
 
 function renderLink (url, title, icon, hidden = '') {
   return `<li>
-<a class="qg-share-link qg-accessibility-off" href="${url}" title="${title}">${icon}<span class="title ${hidden}"">${title}</span></a>
-</li>`;
+            <a class="qg-share-link qg-accessibility-off" href="${url}" title="${title}">${icon}<span class="title ${hidden}"">${title}</span></a>
+          </li>`;
 }
 
 function renderShareButtons () {
   return `<h2>Share:</h2>
-<ul class="navbar navbar-right">
-  ${getLinks('primary')}
-  <li id="shareDropdown" class="dropdown dropdown-menu-right">
-    <button id="shareDropdownToggle" class="qg-share-link noicon" title="share" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span class="fa fa-share-alt fa-2x" aria-hidden="true"></span><span class="qg-visually-hidden">Share</span>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="shareDropdownToggle">
-      ${getLinks('secondary')}
-    </ul>
-  </li>
-</ul>`;
+  <ul class="navbar navbar-right">
+    ${getLinks('primary')}
+   </ul>`;
 }
 
 /**
-* #####################################
-* Controller
-**/
+ * #####################################
+ * Controller
+ **/
 
 function getLinks (type) {
   // Get link list
