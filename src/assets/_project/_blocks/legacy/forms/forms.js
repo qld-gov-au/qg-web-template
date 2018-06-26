3b1bc4683dfce6c24b669e6c61dc7ff9ff2f0868
AsifAmin <asif_udr@yahoo.com>
2018-06-27 09:12:48 +1000

    CMS-84 - added 'jquery.forms.js' from SWE2

--- src/assets/_project/_blocks/legacy/forms/forms.js
+++ src/assets/_project/_blocks/legacy/forms/forms.js
@@ -1356,6 +1356,40 @@ if ( jQuery !== 'undefined' ) {
         this.on( 'change', [this, validationMessage], xorConstraintChangeHandler );
     };
 }(jQuery));
+
+(function( $ ){
+  'use strict';
+
+
+  /* detect required field markers for IE6 */
+  $( 'abbr[title*="required"]' ).addClass( 'required' );
+
+
+  // show/hide entire 'question' when fields become irrelevant
+  $( '.questions > li' ).not( '.section' )
+    .bind( 'relevant', function( event ) {
+      $( this ).relevance( 'show' );
+      event.stopImmediatePropagation();
+    })
+    .bind( 'irrelevant', function( event ) {
+      $( this ).relevance( 'hide' );
+      event.stopImmediatePropagation();
+    })
+  ;
+
+
+  // click the table cell to click on a matrix option
+  $( '.matrix' ).delegate( 'td', 'click', function( evt ) {
+    $( evt.target )
+      .find( 'input' )
+      .trigger( 'click' )
+      .trigger( 'change' )
+    ;
+  });
+
+}( jQuery ));
+
+
 /**
  * This file initialises forms
  */
