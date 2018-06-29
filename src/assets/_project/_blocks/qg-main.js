0ca46d7179456515dfef33e8e7820bc38188032b
AsifAmin <asif_udr@yahoo.com>
2018-06-29 17:24:15 +1000

    [CMS-122]-SWE3 Index page alignment when there's no images for the collection

--- src/assets/_project/_blocks/qg-main.js
+++ src/assets/_project/_blocks/qg-main.js
@@ -47,5 +47,7 @@ import './utils/qg-init';
   feedbackForm.init(franchiseTitle);
   shareLinks.init();
   accessibility.init();
+
+  $('.qg-index-links .qg-index-item img').length === 0 ? $('.qg-index-links').addClass('content-only') : '';
 }());
 
