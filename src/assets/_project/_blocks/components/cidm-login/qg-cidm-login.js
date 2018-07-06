0f45e2d7e7109823c54765fe11029de2c3e31869
ganapavz <vani.ganapa@dsiti.qld.gov.au>
2018-07-06 15:03:28 +1000

    QOL-3735-cidm login with responsive

--- src/assets/_project/_blocks/components/cidm-login/qg-cidm-login.js
+++ src/assets/_project/_blocks/components/cidm-login/qg-cidm-login.js
@@ -6,7 +6,7 @@
     window.cidmLoaderBaseUrl = '';
     let loadCidm, getConfig;
     loadCidm = function (res) {
-      console.log(res);
+      // console.log(res);
       let config = res.config;
       let css = '<link rel="stylesheet" type="text/css" href="' + res.cssURL + '">';
       window.cidmLoaderBaseUrl = res.cidmLoaderBaseUrl;
@@ -19,13 +19,16 @@
               $jc.qgcidm.initialise(config);
               $jc('#qgcidm-avatar').avatar();
               $jc.qgcidm.enable();
+              if (qg.swe.cidmCallback && typeof qg.swe.cidmCallback === 'function') {
+                qg.swe.cidmCallback();
+              }
             });
           });
         });
       }
     };
     getConfig = function () {
-      console.log('Loaded require');
+      // console.log('Loaded require');
       swe.ajaxCall('/assets/apps/cidm/qg-cidm-config.json', 'json', loadCidm, 'failed to load cidm config. Expecting config file at "/assets/apps/widget/qg-cidm-config.json"');
     };
     swe.ajaxCall(window.qg.cdn + window.qg.swe.assets + 'lib/ext/require.min.js', 'script', getConfig, 'Require JS unavailable');
