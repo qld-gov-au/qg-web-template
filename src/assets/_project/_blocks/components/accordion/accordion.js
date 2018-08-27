f0389a59a41144f52bea4129bbc26c1603b05ae3
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2018-08-27 11:23:03 +1000

    Release 3.4.0 (#147)
    * created branch and cleaned content page
    
    * CMS-112-added subtitle to accordion (#145)
    
    * CMS-112-added subtitle to accordion
    
    * CMS-112-replaced p with span
    
    * CMS-112-added expand/collapse
    
    * CMS-112-Ability to direct link to each section and expand the linked section
    
    * updated version

---
+++ src/assets/_project/_blocks/components/accordion/accordion.js
@@ -0,0 +1,33 @@
+/**
+ * This will handle functionalities like
+ * - Expand all / Collapse all link
+ * - Ability to direct link to each section and expand the linked section
+ * - Handles aria-expanded values
+ */
+
+(function ($) {
+  let accordion = '.qg-accordion';
+  let accordionControls = 'input[name=control]';
+  let linkedpanel = $(window.location.hash);
+
+  //Handle events of accordion inputs
+  $(accordion).find('article input').on('change', function () {
+    let checkedStatus = $(this).prop('checked');
+    let controlledPanedId = $('#' + $(this).attr('aria-controls'));
+    $(this)
+    .attr('aria-expanded', checkedStatus) //sets aria
+    .parents(accordion).find(accordionControls).prop('checked', false); //clears expand/collapse selection
+    controlledPanedId.attr('aria-hidden', !checkedStatus);
+  });
+
+  //expand all click
+  $(accordion).find(accordionControls).on('change', function () {
+    $(this).find('~ article input').prop('checked', $(this).val() === 'expand');
+    $(accordion).find('article input').trigger('change');
+  });
+
+  //Ability to direct link to each section and expand the linked section
+  if (linkedpanel) {
+    linkedpanel.prop('checked', true);
+  }
+}(jQuery));
