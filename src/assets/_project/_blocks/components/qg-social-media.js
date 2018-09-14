da2ecd6bb19417c8fd88767532ddbf6066c0ca40
AsifAmin <asif_udr@yahoo.com>
2018-09-14 15:35:34 +1000

    QOL-4055-SWE3 - Fix twitter script

--- src/assets/_project/_blocks/components/qg-social-media.js
+++ src/assets/_project/_blocks/components/qg-social-media.js
@@ -1,29 +1,14 @@
 /*global jQuery*/
 (function ($) {
   'use strict';
-
   var twitter = {
     ele: $('.qg-twitter-updates') || '',
     init: function () {
       if (twitter.ele.length > 0) {
-        var account = twitter.ele.data('account') || '';
-        var list = twitter.ele.data('list') || '';
-        var widgetid = twitter.ele.data('widgetid') || '';
-        var num = twitter.ele.data('num') || 5;
-
-        if (account.length > 0 && widgetid.length > 0) {
-          twitter.generateIframe(account, list, widgetid, num);
-        } else {
-          console.log('data-account/data-widgetid attribute is empty');
-        }
+        twitter.loadScript();
       }
     },
-    generateIframe: function (account, list, widgetid, num) {
-      var html = '<div><a class="twitter-timeline" href="https://twitter.com/' + account + (list.length > 0 ? '/' + list : '') + '" data-widget-id="' + widgetid + '" data-tweet-limit="' + num + '" data-chrome="transparent noheader noborders nofooter" data-link-color="#546A9A">Tweets from @' + account + (list.length > 0 ? '/' + list : '') + '</a></div>';
-      twitter.ele.append(html);
-      twitter.runScript();
-    },
-    runScript: function () {
+    loadScript: function () {
       return !(function (d, s, id) {
         var js;
         var fjs = d.getElementsByTagName(s)[0];
