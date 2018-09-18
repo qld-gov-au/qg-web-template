9ed4103ce9e5ddd4a721e0fb8ac6d05b87936d81
AsifAmin <asif_udr@yahoo.com>
2018-09-18 15:55:59 +1000

    QOL-4014 - SWE3 - facebook feed implementation

--- src/assets/_project/_blocks/components/qg-social-media.js
+++ src/assets/_project/_blocks/components/qg-social-media.js
@@ -1,27 +1,36 @@
 /*global jQuery*/
 (function ($) {
   'use strict';
-  var twitter = {
-    ele: $('.qg-twitter-updates') || '',
+
+  var qgSocialMedia = {
+    config: {
+      $twitterEl: $('.qg-twitter-updates'),
+      $facebookEl: $('.qg-facebook-updates'),
+    },
     init: function () {
-      if (twitter.ele.length > 0) {
-        twitter.loadScript();
+      let twitterSdkScript = 'platform.twitter.com/widgets.js';
+      let facebookSdkScript = 'connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8';
+      if (this.config.$twitterEl.length > 0 && this.config.$twitterEl.find('iframe').length <= 0) {
+        this.loadScript('script', 'twitter-wjs', twitterSdkScript);
+      }
+      if (this.config.$facebookEl.length > 0 && this.config.$facebookEl.find('iframe').length <= 0) {
+        var fbUrl = this.config.$facebookEl.attr('data-href');
+        var fbhtml = '<div class="fb-page" data-href="' + fbUrl + '" data-tabs="timeline" data-small-header="true" data-width="10000"  data-adapt-container-width="true" data-show-facepile="false"></div>';
+        this.config.$facebookEl.append(fbhtml);
+        this.loadScript('script', 'facebook-wjs', facebookSdkScript);
       }
     },
-    loadScript: function () {
-      return !(function (d, s, id) {
-        var js;
-        var fjs = d.getElementsByTagName(s)[0];
-        var p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-          js = d.createElement(s);
-          js.id = id;
-          js.src = p + '://platform.twitter.com/widgets.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }
-      }(document, 'script', 'twitter-wjs'));
+    loadScript: function (tag, id, sdkUrl) {
+      var createEl;
+      var fjs = document.getElementsByTagName(tag)[0];
+      var p = /^http:/.test(document.location) ? 'http' : 'https';
+      if (!document.getElementById(id)) {
+        createEl = document.createElement(tag);
+        createEl.id = id;
+        createEl.src = `${p}://${sdkUrl}`;
+        fjs.parentNode.insertBefore(createEl, fjs);
+      }
     },
-
   };
-  twitter.init();
+  qgSocialMedia.init();
 }(jQuery));
