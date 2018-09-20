dccf33e5167e584d6c41004f5ad1ae6b5c80bc46
AsifAmin <asif_udr@yahoo.com>
2018-09-21 08:14:40 +1000

    QOL-4066 - added social media

--- src/assets/_project/_blocks/components/qg-social-media.js
+++ src/assets/_project/_blocks/components/qg-social-media.js
@@ -10,10 +10,10 @@
     init: function () {
       let twitterSdkScript = 'platform.twitter.com/widgets.js';
       let facebookSdkScript = 'connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8';
-      if (this.config.$twitterEl.length > 0 && this.config.$twitterEl.find('iframe').length <= 0) {
+      if (this.config.$twitterEl.length > 0 && $('script[src*="' + twitterSdkScript + '"]').length <= 0) {
         this.loadScript('script', 'twitter-wjs', twitterSdkScript);
       }
-      if (this.config.$facebookEl.length > 0 && this.config.$facebookEl.find('iframe').length <= 0) {
+      if (this.config.$facebookEl.length > 0 && $('script[src*="' + facebookSdkScript + '"]').length <= 0) {
         var fbUrl = this.config.$facebookEl.attr('data-href');
         var fbhtml = '<div class="fb-page" data-href="' + fbUrl + '" data-tabs="timeline" data-small-header="true" data-width="10000"  data-adapt-container-width="true" data-show-facepile="false"></div>';
         this.config.$facebookEl.append(fbhtml);
@@ -34,3 +34,4 @@
   };
   qgSocialMedia.init();
 }(jQuery));
+
