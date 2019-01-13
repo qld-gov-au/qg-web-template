50eefe4012ba7fba041a1e275bf2bf0f19776553
AsifAmin <asif_udr@yahoo.com>
2019-01-14 08:45:56 +1000

    QOL-4413 - Content pages without left navigation

--- src/assets/_project/_blocks/qg-main.js
+++ src/assets/_project/_blocks/qg-main.js
@@ -46,11 +46,18 @@ import feedbackForm       from './layout/footer/feedback-form';
 import './utils/qg-init';
 (function () {
   'use strict';
-  var franchiseTitle = qg && qg.swe && qg.swe.franchiseTitle;
+  let franchiseTitle = qg && qg.swe && qg.swe.franchiseTitle;
   activeSideNav.highlightNavItem();
   stepNav.init();
   feedbackForm.init(franchiseTitle);
   shareLinks.init();
   accessibility.init();
+
+  // Workaround to push columns - some pages on Matrix have a 3 col layout but there is no third columns
+  // example - https://www.qld.gov.au/about/events-awards-honours/awards/reconciliation-awards/2015-award-winners
+  let $qg = $('#qg-three-col');
+  if ($qg.find('#qg-section-nav').length <= 0) {
+    $qg.addClass('no-secondary-nav');
+  }
 }());
 
