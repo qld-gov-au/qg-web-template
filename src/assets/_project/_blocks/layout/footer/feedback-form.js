f44beec38860e2d850098a51907c04d7dea473f0
AsifAmin <asif_udr@yahoo.com>
2019-02-22 12:25:43 +1000

    QOL-4498 Browser type included in feedback form emails

--- src/assets/_project/_blocks/layout/footer/feedback-form.js
+++ src/assets/_project/_blocks/layout/footer/feedback-form.js
@@ -15,6 +15,23 @@ function sanitize (str) {
     .replace(/{/g, '&#123;') // strip (
     .replace(/}/g, '&#124;'); // strip )
 }
+// the script try to predict browser name from the User-Agent
+let browserName = (function () {
+  let ua = navigator.userAgent;
+  let tem;
+  let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'IE ' + (tem[1] || '');
+  }
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+    if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+  }
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+})();
 
 function addHiddenInput (key, val) {
   var newHiddenInput = $('<input type="hidden"/>');
@@ -28,6 +45,8 @@ function init (franchiseTitle) {
   addHiddenInput('page-url', window.location.href);
   addHiddenInput('page-referer', document.referrer);
   addHiddenInput('rspUsrAgent', navigator.userAgent);
+  addHiddenInput('browserName', browserName);
+  addHiddenInput('OS', navigator.platform);
 }
 
 module.exports = { init: init };
