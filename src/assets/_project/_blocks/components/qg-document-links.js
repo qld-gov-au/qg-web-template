d40828e8df578cf03a622b2ae70f2666f71df97b
AsifAmin <asif_udr@yahoo.com>
2019-04-04 14:26:28 +1000

    QOL-4499 Downloadable document links file size decimals

--- src/assets/_project/_blocks/components/qg-document-links.js
+++ src/assets/_project/_blocks/components/qg-document-links.js
@@ -3,22 +3,20 @@
 
   // Use uppercase here, as not all uses are case sensitive
   var documentTypes = 'PDF|DOC|DOCX|XLS|XLSX|RTF';
-
   // onready
   $(document).ready(function () {
-    // find links in content and asides that are missing metadata markup
     $('a', '#qg-primary-content, #qg-secondary-content').each(function () {
       var $this = $(this);
       var linkText = $this.text();
-      var dtRegex = new RegExp(documentTypes);
-      if (dtRegex.test(linkText)) {
-        if (/\.\d*?/.test(linkText) && /KB/.test(linkText)) {
-          var extractSize = new RegExp('\\((?:' + documentTypes + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
-          $(this).find('.meta').empty().append(linkText.match(extractSize)[0].replace(/(\.\d*)/gi, ''));
-        }
-      } else {
-        linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
+      var dtRegex = new RegExp(documentTypes, 'i');
+      if (dtRegex.test($this.attr('href'))) {
         if (dtRegex.test(linkText)) {
+          if (/\.\d*?/.test(linkText) && /KB/.test(linkText)) {
+            var extractSize = new RegExp('\\((?:' + documentTypes + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
+            $(this).find('.meta').empty().append(linkText.match(extractSize)[0].replace(/(\.\d*)/gi, ''));
+          }
+        } else {
+          linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
           $this.append(' <span class="meta">(' + linkText + ')</span>');
         }
       }
