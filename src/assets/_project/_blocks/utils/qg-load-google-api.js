2705d8dcaa1351477a53be226a46011edb61f2b8
AsifAmin <asif_udr@yahoo.com>
2019-04-10 09:57:27 +1000

    QOL-4700 - transferred static map function from Squiz to SWE3 and further cleaning of the code

--- src/assets/_project/_blocks/utils/qg-load-google-api.js
+++ src/assets/_project/_blocks/utils/qg-load-google-api.js
@@ -2,30 +2,49 @@
  * When using functionality related to google libraries, this fuction comes handy to check if libraries already exists and then execute custom function
  */
 /* globals qg */
-import keys from '../qg-google-keys';
+import keys from '../data/qg-google-keys';
 
 (function (qg, $) {
   'use strict';
+  let googleApiKey;
+  let $mapImg = $('#qg-static-map');
   window.qg.googleKey = window.location.hostname.search(/\bdev\b|\btest\b|\blocalhost\b|\buat\b/) !== -1 ? keys.defGoogle.uat : keys.defGoogle.prod;
   window.qg.googleRecaptchaApiKey = window.location.hostname.search(/\bdev\b|\btest\b|\blocalhost\b|\buat\b/) !== -1 ? keys.defGoogleRecaptcha.uat : keys.defGoogleRecaptcha.prod;
 
-  var findFranchiseName = function () {
-    var path = window.location.pathname.replace(/\/$/, '');
-    var pathArr = path.split('/').filter(function (e) {
+  let findFranchiseName = function () {
+    let path = window.location.pathname.replace(/\/$/, '');
+    let pathArr = path.split('/').filter(function (e) {
       return e;
     });
     return pathArr[0].toLowerCase();
   };
+  let franchise = findFranchiseName();
+  if (franchise) {
+    keys.franchises.forEach(function (e) {
+      if (franchise === e.name) {
+        window.qg.franchise = {
+          name: e.name,
+          apiKey: e.apiKey,
+        };
+      }
+    });
+  }
+  googleApiKey = window.qg.franchise && window.qg.franchise.apiKey ? window.qg.franchise.apiKey : window.qg.googleKey;
 
-  keys.franchises.forEach(function (franchise) {
-    if (findFranchiseName() === franchise.name) {
-      window.qg.franchise = {
-        name: franchise.name,
-        apiKey: franchise.apiKey,
-      };
-    }
-  });
+  function generateStaticMapImg (ele) {
+    let lat = ele.attr('data-lat') || -27.4673;
+    let lon = ele.attr('data-long') || 153.0233;
+    let zoom = ele.attr('data-zoom') || 17;
+    let height = ele.attr('data-height') || 189;
+    return 'https://maps.googleapis.com/maps/api/staticmap?size=373x' + height + '&maptype=roadmap&markers=' + lat + '%2C' + lon + '&key=' + googleApiKey + '&sensor=false&zoom=' + zoom;
+  }
 
+  if ($mapImg.length > 0) {
+    $mapImg.find('img').attr('src', generateStaticMapImg($mapImg.find('img')));
+    let mapClone = $mapImg.clone();
+    $mapImg.remove();
+    $('aside').prepend(mapClone);
+  }
   function lazyScript (url) {
     $('head').append('<script type="text/javascript" src="' + url + '"></script>');
   }
@@ -39,7 +58,6 @@ import keys from '../qg-google-keys';
       }
     };
     if ($('#googleapi').length <= 0) {
-      let googleApiKey = window.qg.franchise && window.qg.franchise.apiKey ? window.qg.franchise.apiKey : window.qg.googleKey;
       let s = document.createElement('script');
       let u = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&region=AU&libraries=places`;
       s.type = 'text/javascript';
