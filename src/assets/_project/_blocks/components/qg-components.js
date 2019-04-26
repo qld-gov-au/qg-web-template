aed3a3ec1775318a0b54231482da16527bdf900d
AsifAmin <asif_udr@yahoo.com>
2019-04-26 15:41:35 +1000

    QOL-4469 Move image gallery as a component in SWE3

---
+++ src/assets/_project/_blocks/components/qg-components.js
@@ -0,0 +1,13 @@
+import './qg-social-media';
+import './progressive-reveal';
+import './license';
+import './carousel/carousel';
+import './quick-exit/quick-exit';
+import './accordion/accordion';
+import './forms/recaptcha';
+import './forms/qg-address-autocomplete';
+import './qg-document-links';
+import './gallery/qg-gallery';
+import accessibility      from './accessibility/accessibility';
+
+accessibility.init();
