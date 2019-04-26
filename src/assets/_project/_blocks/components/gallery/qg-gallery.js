2254be6cc6a887fe7749c8ad32e8eb3d92180dda
AsifAmin <asif_udr@yahoo.com>
2019-04-26 16:36:34 +1000

    QOL-4469 code cleaning

--- src/assets/_project/_blocks/components/gallery/qg-gallery.js
+++ src/assets/_project/_blocks/components/gallery/qg-gallery.js
@@ -3,27 +3,29 @@ if ($('.qg-image-gallery')) {
   load(['{{ON-RELEASE-CDN}}/assets/v3.1/latest/lib/ext/fancybox/jquery.fancybox.min.css', '{{ON-RELEASE-CDN}}/assets/v3.1/latest/lib/ext/fancybox/jquery.fancybox.min.js'], function () {
     $('[data-fancybox^="gallery"]').fancybox({
       buttons: ['thumbs', 'close'],
-      baseTpl:
-        '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-        '<div class="fancybox-bg"></div>' +
-        '<div class="fancybox-inner">' +
-        '<div class="fancybox-infobar"><button data-fancybox-prev="" class="fancybox-button fancybox-button--arrow_left" title="Previous"><div><span class="font-awesome fa-2x fa-caret-left"></span></div></button><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span><button data-fancybox-next="" class="fancybox-button fancybox-button--arrow_right" title="Next"><div><span class="font-awesome fa-2x fa-caret-right"></span></div></button></div>' +
-        '<div class="fancybox-toolbar">{{buttons}}</div>' +
-        '<div class="fancybox-navigation">{{arrows}}</div>' +
-        '<div class="fancybox-stage"></div>' +
-        '<div class="fancybox-caption">' +
-        '</div>' +
-        '</div>' +
-        '</div>',
+      baseTpl: `
+        <div class="fancybox-container" role="dialog" tabindex="-1">
+          <div class="fancybox-bg"></div>
+          <div class="fancybox-inner">
+                <div class="fancybox-infobar"><button data-fancybox-prev="" class="fancybox-button fancybox-button--arrow_left" title="Previous"><div><span class="font-awesome fa-2x fa-caret-left"></span></div></button><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span><button data-fancybox-next="" class="fancybox-button fancybox-button--arrow_right" title="Next"><div><span class="font-awesome fa-2x fa-caret-right"></span></div></button></div>
+                <div class="fancybox-toolbar">{{buttons}}</div>
+                <div class="fancybox-navigation">{{arrows}}</div>
+                <div class="fancybox-stage"></div>
+                <div class="fancybox-caption"></div>
+          </div>
+        </div>
+      `,
       btnTpl: {
-        arrowLeft:
-          '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
-          '<div><span class="font-awesome fa-2x fa-caret-left"></span></div>' +
-          '</button>',
-        arrowRight:
-          '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
-          '<div><span class="font-awesome fa-2x fa-caret-right"></span></div>' +
-          '</button>',
+        arrowLeft: `
+          <button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">
+            <div><span class="font-awesome fa-2x fa-caret-left"></span></div>
+          </button>
+        `,
+        arrowRight: `
+           <button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">
+             <div><span class="font-awesome fa-2x fa-caret-right"></span></div>
+          </button>
+        `,
       },
       caption: function (instance, item) {
         var caption = $(this).data('caption') || '';
