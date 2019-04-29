d946585ee3439658e6c2b37ecb9a0c9c5c70f96d
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2019-04-30 08:52:24 +1000

    Qol 4469 image gallery swe3 (#176)
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning

--- gulp/common-tasks/js.js
+++ gulp/common-tasks/js.js
@@ -35,6 +35,15 @@ module.exports = function (gulp, plugins, config, webpack, destFolder, type = 'b
           },
         },
         {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'webpack-replace',
+          query: {
+            search: '{{CDN}}',
+            replace: process.env.NODE_ENV === 'prod' ? `https://static.qgov.net.au` : '',
+          },
+        },
+        {
           test: /\.json$/,
           loader: 'json-loader',
         },
--- src/assets/_project/_blocks/utils/qg-init.js
+++
@@ -1,3 +0,0 @@
-/**
- * This file contains general initialisation, element eventlisteners etc
- */
