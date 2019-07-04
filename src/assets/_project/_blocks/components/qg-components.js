0b8f89be226b71e87cc8d73501cb660e3c167878
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2019-07-04 15:16:08 +1000

    Release v3.1.5 (#179)
    * created release 3.1.5 branch
    
    * Qol 4469 image gallery swe3 (#176)
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * Qol 4879 enhancements to figure styles (#177)
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4879 - Enhancements to Figure styles
    
    * recent changes
    
    * recent changes (#178)
    
    * QOL-4778 - SWE 3.1.5
    
    * Added static maps function
    
    * Added base template screenshots

---
+++ src/assets/_project/_blocks/components/qg-components.js
@@ -0,0 +1,12 @@
+import './qg-social-media';
+import './progressive-reveal';
+import './license';
+import './carousel/carousel';
+import './quick-exit/quick-exit';
+import './accordion/accordion';
+import './forms/recaptcha';
+import './forms/qg-address-autocomplete';
+import './qg-document-links';
+import accessibility      from './accessibility/accessibility';
+
+accessibility.init();
