afe4ac98e20637e873ebd1f8e38007181254e7a6
andersmx <44218292+andersmx@users.noreply.github.com>
2019-07-24 15:56:57 +1000

    Qol 5384 recaptcha fix (#183)
    * Release v3.1.5 (#179)
    
    * created release 3.1.5 branch
    
    * Qol 4469 image gallery swe3 (#176)
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * Qol 4879 enhancements to figure styles (#177)
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469
    Move image gallery as a component in SWE3
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4469 code cleaning
    
    * QOL-4879 - Enhancements to Figure styles
    
    * recent changes
    
    * recent changes (#178)
    
    * QOL-4778 - SWE 3.1.5
    
    * Added static maps function
    
    * Added base template screenshots
    
    * Updated packages - there was a security alert from Github in these 2 packages version (#180)
    
    * QOL-5384 Fix to recaptcha, requires reset after first init
    
    * QOL-5384 Fix

--- src/assets/_project/_blocks/components/forms/recaptcha.js
+++ src/assets/_project/_blocks/components/forms/recaptcha.js
@@ -13,25 +13,28 @@ import keys from '../../data/qg-google-keys';
   let onloadRecaptcha = () => { // eslint-disable-line
     $('form[data-recaptcha="true"]').find('input[type="submit"], button[type="submit"]').on('click', (e) => {
       e.preventDefault();
+      let recaptchaInit = $('li.footer ul.actions div.grecaptcha-badge');
       let subBtn = e.target;
       let form = $(subBtn).parents('form');
-
-      grecaptcha.render(subBtn, {
-        'sitekey': googleRecaptchaApiKey,
-        'callback': () => {
-          var response = grecaptcha.getResponse();
-          if (response === '' || response === undefined || response.length === 0) {
-            console.log('Invalid recaptcha');
-            return false;
-          } else {
-            form.submit();
-          }
-        },
-      });
-      grecaptcha.execute();
+        if (recaptchaInit.length) {
+          grecaptcha.reset();
+        } else {
+          grecaptcha.render(subBtn, {
+            'sitekey': googleRecaptchaApiKey, //this value will be replaced by build tool. from gulp-config/
+            'callback': () => {
+              var response = grecaptcha.getResponse();
+              if (response === '' || response === undefined || response.length === 0) {
+                console.log('Invalid recaptcha');
+                return false;
+              } else {
+                form.submit();
+              }
+            },
+          });
+        }
+        grecaptcha.execute();
     });
   };
-
   if ($('form[data-recaptcha="true"]').length > 0) {	//enable recaptcha on form submits
     swe.ajaxCall('https://www.google.com/recaptcha/api.js', 'script', onloadRecaptcha, 'Recaptcha unavailable');
   }
