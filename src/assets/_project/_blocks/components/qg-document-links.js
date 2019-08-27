d8f69c7bc8361fa54099a37c653295e6a53b0cd1
AsifAmin <asif_udr@yahoo.com>
2019-08-27 15:37:47 +1000

    DC-6917 - duplicate document type issue fixed

--- src/assets/_project/_blocks/components/qg-document-links.js
+++ src/assets/_project/_blocks/components/qg-document-links.js
@@ -2,21 +2,24 @@
   'use strict';
   let linkType = '.PDF$|.DOC$|.DOCX$|.XLS$|.XLSX$|.RTF$';
   let contentType = 'PDF|DOC|DOCX|XLS|XLSX|RTF';
-  // onready
   $(document).ready(function () {
     $('a', '#qg-primary-content, #qg-secondary-content').each(function () {
       let $this = $(this);
       let linkRegex = new RegExp(linkType, 'i');
-      let contentRegex = new RegExp(contentType, 'i');
+      // check to see if a link with a selected linkType exist
+      // Example - cue-template-change-log.pdf|rtf...
       if (linkRegex.test($this.attr('href'))) {
-        let linkText = $this.text();
-        if (contentRegex.test(linkText)) {
-          if (/\.\d*?/.test(linkText) && /KB/.test(linkText)) {
-            let extractSize = new RegExp('\\((?:' + contentType.toUpperCase() + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
-            linkText.match(extractSize) ? $(this).find('.meta').empty().append(linkText.match(extractSize)[0].toUpperCase().replace(/(\.\d*)/gi, '')) : '';
-          }
-        } else {
-          linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
+        let contentRegex = new RegExp(contentType);
+        let currContent = $this.text();
+        if (/\.\d*?/.test(currContent)) {
+          // check to see if decimals exist, if yes then round then off
+          // Example (PDF 106.66) -> (PDF 106)
+          let extractSize = new RegExp('\\((?:' + contentType.toUpperCase() + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
+          currContent.match(extractSize) ? $(this).find('.meta').empty().append(currContent.match(extractSize)[0].toUpperCase().replace(/(\.\d*)/gi, '')) : '';
+        } else if (!contentRegex.test(currContent)) {
+          // check to see there is no doc type present in the content section
+          // If yes then insert <span class="meta">PDF</span>
+          let linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
           $this.append(' <span class="meta">(' + linkText + ')</span>');
         }
       }
