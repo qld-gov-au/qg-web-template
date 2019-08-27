5c7eace66ee373b4f366e2f5e78675c0626770b7
AsifAmin <asif_udr@yahoo.com>
2019-08-27 14:21:27 +1000

    revert document links change

--- src/assets/_project/_blocks/components/qg-document-links.js
+++ src/assets/_project/_blocks/components/qg-document-links.js
@@ -7,15 +7,16 @@
     $('a', '#qg-primary-content, #qg-secondary-content').each(function () {
       let $this = $(this);
       let linkRegex = new RegExp(linkType, 'i');
-      let contentRegex = new RegExp(contentType);
-      let currContent = $this.text();
+      let contentRegex = new RegExp(contentType, 'i');
       if (linkRegex.test($this.attr('href'))) {
-        if (/\.\d*?/.test(currContent)) {
-          console.log('found dcimals', currContent);
-          let extractSize = new RegExp('\\((?:' + contentType.toUpperCase() + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
-          currContent.match(extractSize) ? $(this).find('.meta').empty().append(currContent.match(extractSize)[0].toUpperCase().replace(/(\.\d*)/gi, '')) : '';
-        } else if (!contentRegex.test(currContent)) {
-          let linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
+        let linkText = $this.text();
+        if (contentRegex.test(linkText)) {
+          if (/\.\d*?/.test(linkText) && /KB/.test(linkText)) {
+            let extractSize = new RegExp('\\((?:' + contentType.toUpperCase() + '),?\\s+[0-9\\.]+\\s*[KM]B\\)', 'i');
+            linkText.match(extractSize) ? $(this).find('.meta').empty().append(linkText.match(extractSize)[0].toUpperCase().replace(/(\.\d*)/gi, '')) : '';
+          }
+        } else {
+          linkText = $this.attr('href').replace(/^.*\.(.+)$/, '$1').toUpperCase();
           $this.append(' <span class="meta">(' + linkText + ')</span>');
         }
       }
