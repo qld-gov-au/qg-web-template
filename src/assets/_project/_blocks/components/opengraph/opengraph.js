388fd538f822a27684085662237cf794c32dbb73
Mitchell Anderson <mitchell.anderson@smartservice.qld.gov.au>
2019-10-15 10:15:38 +1000

    QOL-5801 Updates to opengraph tagging

--- src/assets/_project/_blocks/components/opengraph/opengraph.js
+++ src/assets/_project/_blocks/components/opengraph/opengraph.js
@@ -4,30 +4,49 @@
         {property: 'meta[property="og:title"]'},
         {property: 'meta[property="og:description"]'},
         {property: 'meta[property="og:url"]'},
+        {property: 'meta[property="og:type"]'},
+        {property: 'meta[property="og:image"]'},
         {property: 'meta[name="twitter:card"]'},
         {property: 'meta[name="twitter:site"]'},
-        {property: 'meta[name="twitter:creator"]'}
+        {property: 'meta[name="twitter:title"]'},
+        {property: 'meta[name="twitter:description"]'},
+        {property: 'meta[name="twitter:image"]'}
     ];
     const openGraph = {
         init: function () {
+            let graphImg = '/assets/v3.1/latest/images/coat-of-arms/coa-thumbnail.png';
+            let descriptionMeta = $('meta[name="DCTERMS.description"]').attr('content');
             $.each(fields, function (key, val) {
                 let itemObj = $(val.property);
                 if (itemObj.length > 0) {
+                    //check if template not already populated by Matrix or authorer
                     if (itemObj.attr('content') === '' || itemObj.attr('content') === undefined) {
-                        if (itemObj.attr('property') === 'og:title') {
+                        if (itemObj.attr('property') === 'og:title' || itemObj.attr('name') === 'twitter:title') {
                             itemObj.attr('content', document.title);
-                            return true;
                         }
                         if (itemObj.attr('property') === 'og:url') {
                             itemObj.attr('content', window.location.href);
-                            return true;
                         }
-                        if (itemObj.attr('property') === 'og:description') {
-                            let descriptionMeta = $('meta[name="DCTERMS.description"]').attr('content');
-                            itemObj.attr('content', descriptionMeta);
-                            return true;
+                        if (itemObj.attr('property') === 'og:type') {
+                            itemObj.attr('content', 'article');
+                        }
+                        if (itemObj.attr('name') === 'twitter:card') {
+                            itemObj.attr('content', 'summary');
+                        }
+                        if (itemObj.attr('name') === 'twitter:site') {
+                            itemObj.attr('content', '@QldGov');
+                        }
+                        if (itemObj.attr('property') === 'og:description' || itemObj.attr('name') === 'twitter:description') {
+                            if (descriptionMeta.length > 0) {
+                                itemObj.attr('content', descriptionMeta);
+                            }
+                        }
+                        if (itemObj.attr('property') === 'og:image' || itemObj.attr('name') === 'twitter:image') {
+                            itemObj.attr('content', graphImg);
                         }
                     }
+                } else {
+                    $('head').appendTo(itemObj).attr('content', '');
                 }
             });
         }
