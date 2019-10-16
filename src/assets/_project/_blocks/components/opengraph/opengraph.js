b330f0ffe9f5dc76b51c144284b2e7389599b6e4
Mitchell Anderson <mitchell.anderson@smartservice.qld.gov.au>
2019-10-16 17:24:44 +1000

    QOL-5801 Opengraph

--- src/assets/_project/_blocks/components/opengraph/opengraph.js
+++ src/assets/_project/_blocks/components/opengraph/opengraph.js
@@ -7,7 +7,6 @@
         {property: 'meta[property="og:type"]'},
         {property: 'meta[property="og:image"]'},
         {property: 'meta[name="twitter:card"]'},
-        {property: 'meta[name="twitter:site"]'},
         {property: 'meta[name="twitter:title"]'},
         {property: 'meta[name="twitter:description"]'},
         {property: 'meta[name="twitter:image"]'}
@@ -15,7 +14,7 @@
     const openGraph = {
         init: function () {
             let graphImg = '/assets/v3.1/latest/images/coat-of-arms/coa-thumbnail.png';
-            let descriptionMeta = $('meta[name="DCTERMS.description"]').attr('content');
+            let descriptionMeta = $('meta[name="DCTERMS.description"]').attr('content', '');
             $.each(fields, function (key, val) {
                 let itemObj = $(val.property);
                 if (itemObj.length > 0) {
@@ -23,6 +22,14 @@
                     if (itemObj.attr('content') === '' || itemObj.attr('content') === undefined) {
                         if (itemObj.attr('property') === 'og:title' || itemObj.attr('name') === 'twitter:title') {
                             itemObj.attr('content', document.title);
+                        }
+                        if (itemObj.attr('property') === 'og:description' || itemObj.attr('name') === 'twitter:description') {
+                            if (descriptionMeta.length > 0) {
+                                itemObj.attr('content', descriptionMeta);
+                            }
+                        }
+                        if (itemObj.attr('property') === 'og:image' || itemObj.attr('name') === 'twitter:image') {
+                            itemObj.attr('content', graphImg);
                         }
                         if (itemObj.attr('property') === 'og:url') {
                             itemObj.attr('content', window.location.href);
@@ -33,20 +40,10 @@
                         if (itemObj.attr('name') === 'twitter:card') {
                             itemObj.attr('content', 'summary');
                         }
-                        if (itemObj.attr('name') === 'twitter:site') {
-                            itemObj.attr('content', '@QldGov');
-                        }
-                        if (itemObj.attr('property') === 'og:description' || itemObj.attr('name') === 'twitter:description') {
-                            if (descriptionMeta.length > 0) {
-                                itemObj.attr('content', descriptionMeta);
-                            }
-                        }
-                        if (itemObj.attr('property') === 'og:image' || itemObj.attr('name') === 'twitter:image') {
-                            itemObj.attr('content', graphImg);
-                        }
                     }
                 } else {
-                    $('head').appendTo(itemObj).attr('content', '');
+                    let head = $('head');
+                    head.append(itemObj);
                 }
             });
         }
