24479d1581035ca861830a2564983b0fe7cc5823
AsifAmin <asif_udr@yahoo.com>
2019-10-29 14:16:45 +1000

    improved gallery component

--- src/assets/_project/_blocks/components/gallery/qg-gallery.js
+++ src/assets/_project/_blocks/components/gallery/qg-gallery.js
@@ -1,33 +1,32 @@
-$(window).on('load', function () {
-  if ($("script[src*='jquery.fancybox']").length === 0) {
-    if ($('.qg-image-gallery') || $('.qg-lightbox')) {
-      $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/fancybox/jquery.fancybox.min.css' type='text/css' media='screen' />"));
-      $.getScript('{{CDN}}/latest/lib/ext/fancybox/jquery.fancybox.min.js', function () {
-        // image gallery
-        $('.qg-image-gallery').each(function (index) {
-          $(this).find('a').each(function () {
-            if (!$(this).is('[data-fancybox]')) {
-              $(this).attr('data-fancybox', `gallery-${index}`);
-            }
-            if (!$(this).is('[data-caption]')) {
-              $(this).attr('data-caption', $(this).attr('title'));
-            }
-          });
+if ($("script[src*='jquery.fancybox']").length === 0) {
+  if ($('.qg-image-gallery').length > 0 || $('.qg-lightbox').length > 0 || $('.image-gallery').length > 0 || $('.cut-in').length > 0) {
+    $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/fancybox/jquery.fancybox.min.css' type='text/css' media='screen' />"));
+    $.getScript('{{CDN}}/latest/lib/ext/fancybox/jquery.fancybox.min.js', function () {
+      // image gallery
+      $('.qg-image-gallery, .image-gallery').each(function (index) {
+        $(this).find('a').each(function () {
+          if (!$(this).is('[data-fancybox]')) {
+            $(this).attr('data-fancybox', `gallery-${index}`);
+          }
+          if (!$(this).is('[data-caption]')) {
+            $(this).attr('data-caption', $(this).attr('title'));
+          }
         });
-        // cut in images caption
-        var cutInLink = $('.cut-in .caption a');
-        $(cutInLink).attr('data-fancybox', 'images');
+      });
+      // cut in images caption
+      var cutInLink = $('.cut-in .caption a');
+      $(cutInLink).attr('data-fancybox', 'images');
 
-        $('[data-fancybox^="gallery"]').fancybox({
-          buttons: ['thumbs', 'close'],
-          mobile: {
-            preventCaptionOverlap: false,
-            idleTime: false,
-            clickSlide: function (current, event) {
-              return current.type === 'image' ? 'close' : 'close';
-            },
+      $('[data-fancybox^="gallery"]').fancybox({
+        buttons: ['thumbs', 'close'],
+        mobile: {
+          preventCaptionOverlap: false,
+          idleTime: false,
+          clickSlide: function (current, event) {
+            return current.type === 'image' ? 'close' : 'close';
           },
-          baseTpl: `
+        },
+        baseTpl: `
         <div class="fancybox-container" role="dialog" tabindex="-1">
           <div class="fancybox-bg"></div>
           <div class="fancybox-inner">
@@ -35,33 +34,31 @@ $(window).on('load', function () {
                 <div class="fancybox-toolbar">{{buttons}}</div>
                 <div class="fancybox-navigation">{{arrows}}</div>
                 <div class="fancybox-stage"></div>
-                <div class="fancybox-caption"></div>
+                <div class="fancybox-caption"><div class=""fancybox-caption__body"></div></div>
           </div>
         </div>
       `,
-          btnTpl: {
-            arrowLeft: `
+        btnTpl: {
+          arrowLeft: `
           <button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left p-0" title="{{PREV}}">
             <span class="font-awesome fa-2x fa-caret-left"></span>
           </button>
         `,
-            arrowRight: `
+          arrowRight: `
            <button data-fancybox-next class="fancybox-button fancybox-button--arrow_right p-0" title="{{NEXT}}">
             <span class="font-awesome fa-2x fa-caret-right"></span>
           </button>
         `,
-          },
-          caption: function (instance, item) {
-            var caption = $(this).data('caption') || '';
+        },
+        caption: function (instance, item) {
+          var caption = $(this).data('caption') || '';
 
-            if (item.type === 'image') {
-              caption = '<div class="fancybox-border">' + (caption.length ? caption : '') + '</div>';
-            }
-            return caption;
-          },
-        });
+          if (item.type === 'image') {
+            caption = '<div class="fancybox-border">' + (caption.length ? caption : '') + '</div>';
+          }
+          return caption;
+        },
       });
-    }
+    });
   }
-});
-
+}
--- src/assets/_project/_blocks/components/tables/index.js
+++ src/assets/_project/_blocks/components/tables/index.js
@@ -2,7 +2,7 @@
   'use strict';
   // tables scrollable based on width
   function tablesscrollable () {
-    var $contentTable = $('#qg-primary-content table tbody');
+    var $contentTable = $('#qg-primary-content table');
     if ($contentTable.width() > $('#qg-primary-content').width()) {
       $contentTable.wrap(
         '<div class="scrollable"><div class="inner"></div></div>'
