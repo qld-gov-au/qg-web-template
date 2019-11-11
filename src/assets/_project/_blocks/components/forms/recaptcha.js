9470e99d9150d88dda0d9a99e7120d6b4aa27bd7
Mitchell Anderson <mitchell.anderson@smartservice.qld.gov.au>
2019-11-11 10:37:40 +1000

    QOL-5592 Updated docs and component work

--- src/assets/_project/_blocks/components/forms/recaptcha.js
+++ src/assets/_project/_blocks/components/forms/recaptcha.js
@@ -1,12 +1,12 @@
 /*
-* Any form with form attribute data-recaptcha="true", will run and validate with Google invisible recaptcha
+* Any form with form attribute data-recaptcha='true', will run and validate with Google invisible recaptcha
 * The site key, will be replaced
 *   - Local - by test key in build process (gulp/gulp-config.js, gulp/common-tasks/js.js)
 *   - Dev, Test, Staging, Beta - in bamboo deployment plan - https://servicesmadesimpler.govnet.qld.gov.au/bitbucket/projects/CDN/repos/static-qld_cloudformation/browse/deployment_swev3.yml
 * */
 
 /*globals grecaptcha, qg*/
-import keys from "../../data/qg-google-keys";
+import keys from '../../data/qg-google-keys';
 
 (function($, swe) {
   let googleRecaptchaApiKey =
@@ -17,27 +17,31 @@ import keys from "../../data/qg-google-keys";
       : keys.defGoogleRecaptcha.prod;
   let onloadRecaptcha = () => {
     // eslint-disable-line
-    grecaptcha
-      .execute(googleRecaptchaApiKey, { action: 'homepage' })
-      .then(function(token) {
-        $('form[data-recaptcha="true"]')
-          .find('input[type="submit"], button[type="submit"]')
-          .on('click', e => {
-            e.preventDefault();
-            let subBtn = e.target;
-            let form = $(subBtn).parents('form');
-            var greptcha = form.find('input[name="g-recaptcha-response" type="hidden"]');
-            if (greptcha.length > 0) {
-                greptcha.attr('value', token);
-            } else {
-              var gKey = $('<input type="hidden"/>');
-              gKey.attr('name', 'g-recaptcha-response');
-              gKey.attr('value', token);
-              form.append(gKey);
-            }
-            form.submit();
-          });
+    grecaptcha.ready(function() {
+      grecaptcha
+        .execute(googleRecaptchaApiKey, { action: 'homepage' })
+        .then(function(token) {
+          $('form[data-recaptcha="true"]')
+            .find('input[type="submit"], button[type="submit"]')
+            .on('click', e => {
+              e.preventDefault();
+              let subBtn = e.target;
+              let form = $(subBtn).parents('form');
+              var greptcha = form.find(
+                'input[name="g-recaptcha-response"]'
+              );
+              if (greptcha.length > 0) {
+                greptcha.val(token);
+              } else {
+                var gKey = $('<input type="hidden"/>');
+                gKey.attr('name', 'g-recaptcha-response');
+                gKey.val(token);
+                form.append(gKey);
+              }
+              form.submit();
+            });
       });
+    });
   };
   if ($('form[data-recaptcha="true"]').length > 0) {
     //enable recaptcha on form submits
