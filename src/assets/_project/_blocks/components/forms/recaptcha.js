74537e5c8411f336f0e5c3e28d91afccf88dd543
William Dutton <william.dutton@qld.gov.au>
2019-11-14 16:50:07 +1000

    QOL-6078 need to load multiple onrenders for v3 keys if they exist on the page

--- src/assets/_project/_blocks/components/forms/recaptcha.js
+++ src/assets/_project/_blocks/components/forms/recaptcha.js
@@ -69,42 +69,74 @@ import keys from '../../data/qg-google-keys';
     grecaptcha.execute();
   };
 
+  let loadedRecaptcha = false;
   let onloadRecaptcha = () => {
     grecaptcha.ready(function () {
     // eslint-disable-line
     //v2 Forms
-      $('form[data-recaptcha="true"]')
-        .find('input[type="submit"], button[type="submit"]')
-        .on('click', e => {
-          e.preventDefault();
-          let subBtn = e.target;
-          let form = $(subBtn).parents('form');
-          var greptcha = form.find('input[name="g-recaptcha-response"]');
-          let manualSitekey = form.attr('data-sitekey');
-          let manualAction = form.attr('data-action');
-          if (form.attr('id') === 'qg-page-feedback-form') { //Footer feedback
-            v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, 'feedback');
-          } else if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
-            v3Captcha(form, greptcha, manualSitekey, manualAction);
-          } else if (manualAction !== undefined) { //v3 manual with feedback key but differnt action
-            v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, manualAction);
-          } else if (manualSitekey !== undefined && manualAction === undefined) { //v2 manual (no action in v2)
-            v2Captcha(form, subBtn, manualSitekey);
-          } else { //default v2 with default key
-            v2Captcha(form, subBtn, googleRecaptchaApiKey);
-          }
-        });
+      if (!loadedRecaptcha) {
+        $('form[data-recaptcha="true"]')
+          .find('input[type="submit"], button[type="submit"]')
+          .on('click', e => {
+            e.preventDefault();
+            let subBtn = e.target;
+            let form = $(subBtn).parents('form');
+            var greptcha = form.find('input[name="g-recaptcha-response"]');
+            let manualSitekey = form.attr('data-sitekey');
+            let manualAction = form.attr('data-action');
+            if (form.attr('id') === 'qg-page-feedback-form') { //Footer feedback
+              v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, 'feedback');
+            } else if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
+              v3Captcha(form, greptcha, manualSitekey, manualAction);
+            } else if (manualAction !== undefined) { //v3 manual with feedback key but differnt action
+              v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, manualAction);
+            } else if (manualSitekey !== undefined && manualAction === undefined) { //v2 manual (no action in v2)
+              v2Captcha(form, subBtn, manualSitekey);
+            } else { //default v2 with default key
+              v2Captcha(form, subBtn, googleRecaptchaApiKey);
+            }
+          });
+        loadedRecaptcha = true;
+      }
     });
   };
+  //https://github.com/google/recaptcha/issues/279
+  //https://github.com/google/recaptcha/issues/281
+  //https://www.hackviking.com/development/multiple-recaptcha-on-the-same-page/
   //Setup recaptcha if on the page
   if ($('form[data-recaptcha="true"]').length > 0) {
     //enable recaptcha on form submits, load latest v3 version of recaptcha
-    swe.ajaxCall(
-      'https://www.google.com/recaptcha/api.js?render=' + footerFeedbackGoogleRecaptchaApiKey,
-      'script',
-      onloadRecaptcha,
-      'Recaptcha unavailable'
-    );
+    let v2Loaded = false;
+    $('form[data-recaptcha="true"]').each(function () {
+      let manualSitekey = $(this).attr('data-sitekey');
+      let manualAction = $(this).attr('data-action');
+      if ($(this).attr('id') === 'qg-page-feedback-form') { //Footer feedback
+        swe.ajaxCall(
+          'https://www.google.com/recaptcha/api.js?render=' + footerFeedbackGoogleRecaptchaApiKey,
+          'script',
+           onloadRecaptcha,
+          'Recaptcha unavailable'
+        );
+      } else if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
+        swe.ajaxCall(
+          'https://www.google.com/recaptcha/api.js?render=' + manualSitekey,
+          'script',
+           onloadRecaptcha,
+          'Recaptcha unavailable'
+        );
+      } else {
+        if (!v2Loaded) {
+          swe.ajaxCall(
+            'https://www.google.com/recaptcha/api.js',
+            'script',
+            onloadRecaptcha,
+            'Recaptcha unavailable'
+          );
+          v2Loaded = true;
+        }
+      }
+    });
+
     //If all forms have captchaPrivacyTerms, we can hide reCAPTCHA Badge
     if ($('p[class="captchaPrivacyTerms"]').length === $('form[data-recaptcha="true"]').length) {
       var hidegrecaptchaBadge = '.grecaptcha-badge { visibility: hidden; }';
