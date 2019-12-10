e0044f2c3d638c3444d658f5d49c5a77dd8a2513
AsifAmin <asif_udr@yahoo.com>
2019-12-10 10:59:01 +1000

    QOL-5137 - Rollout Quick exit developed in DFV as Global component

--- src/assets/_project/_blocks/components/quick-exit/quick-exit.js
+++ src/assets/_project/_blocks/components/quick-exit/quick-exit.js
@@ -1,35 +1,27 @@
 (function ($) {
   'use strict';
   var quickExit = {
-    el: '#qg-quick-exit',
+    el: '.qg-quick-exit',
     init: function () {
-      $(this.el).empty().append(this.template);
       this.methods();
     },
-    template: '<header><strong>Quick exit</strong></header><ul><li><a target="_top" data-accesskey="Esc" href="http://www.abc.net.au/tv/epg/#/" title="ABC"><img src="https://www.qld.gov.au/_resources/images/icons/abc-bw.png" alt="ABC"></a></li></ul><footer><strong>press \'Esc\'</strong></footer>',
     methods: function () {
-      var quickExitLinks = $(this.el).find('a');
-      var escLink = $(this.el).find('a[data-accesskey="Esc"]').attr('href');
+      var newloc = 'https://www.google.com.au';
       // action on esc key press
 
       if ($(this.el).length > 0) {
         $(document).keydown(function (e) {
           if (e.keyCode === 27) {
-            window.location.replace(escLink);
+            window.open(newloc, '_blank', '');
+            window.location.replace(newloc);
             return false;
           }
         });
 
-        // clicking on the quick exit block
-        $(document).on('click', this.el, function () {
-          window.location.replace(escLink);
-        });
-
-        //clicking on the links inside the quick exit block
-        quickExitLinks.click(function (e) {
-          e.stopPropagation();
-          e.preventDefault();
-          window.location.replace($(this).attr('href'));
+        // clicking on the quick exit button
+        $('body').on('click', '.qg-quick-exit__button', function () {
+          window.open(newloc, '_blank', '');
+          window.location.replace(newloc);
         });
       }
     },
