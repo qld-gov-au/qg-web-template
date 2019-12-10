9276d10488a51e9ef2208effce115bf2309250cd
AsifAmin <asif_udr@yahoo.com>
2019-12-10 15:15:36 +1000

    Added Quick exit tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   globalSetup: './e2e/config/setup.js',
   globalTeardown: './e2e/config/teardown.js',
   testEnvironment: './e2e/config/puppeteer_environment.js',
+  reporters: ['default', 'jest-junit'],
 };
 
