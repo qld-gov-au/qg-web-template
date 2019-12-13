ff21bd178b393ec50e90737c3d859046d1f9e5e4
AsifAmin <asif_udr@yahoo.com>
2019-12-13 15:13:44 +1000

    quick exit IE11 fix

--- src/assets/_project/_blocks/components/quick-exit/quick-exit.js
+++ src/assets/_project/_blocks/components/quick-exit/quick-exit.js
@@ -1,30 +1,27 @@
-(function ($) {
-  'use strict';
-  var quickExit = {
-    el: '.qg-quick-exit',
-    init: function () {
-      this.methods();
-    },
-    methods: function () {
-      var newloc = 'https://www.google.com.au';
-      // action on esc key press
-
-      if ($(this.el).length > 0) {
-        $(document).keydown(function (e) {
-          if (e.keyCode === 27) {
-            window.open(newloc, '_blank', '');
-            window.location.replace(newloc);
-            return false;
-          }
-        });
-
-        // clicking on the quick exit button
-        $('body').on('click', '.qg-quick-exit__button', function () {
+var quickExit = {
+  el: '.qg-quick-exit',
+  init: function () {
+    this.methods();
+  },
+  methods: function () {
+    var newloc = 'https://www.google.com.au';
+    if ($(this.el).length > 0) {
+      // IE 11 fix
+      require('../../../../../../node_modules/stickyfilljs').add($(this.el));
+      // navigating on pressing Escape key
+      $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
           window.open(newloc, '_blank', '');
           window.location.replace(newloc);
-        });
-      }
-    },
-  };
-  quickExit.init();
-}(jQuery));
+          return false;
+        }
+      });
+      // clicking on the quick exit button
+      $('body').on('click', '.qg-quick-exit__button', function () {
+        window.open(newloc, '_blank', '');
+        window.location.replace(newloc);
+      });
+    }
+  },
+};
+quickExit.init();
