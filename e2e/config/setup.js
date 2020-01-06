ec02966f8530def4604a672b84dfd9337b749557
AsifAmin <asif_udr@yahoo.com>
2020-01-06 11:14:08 +1000

    initial commit

--- e2e/config/setup.js
+++ e2e/config/setup.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
-module.exports = async function() {
+module.exports = async function () {
   console.log(chalk.green('Setup Puppeteer'));
   const browser = await puppeteer.launch({
     executablePath: './node_modules/chromium/lib/chromium/chrome-linux/chrome',
