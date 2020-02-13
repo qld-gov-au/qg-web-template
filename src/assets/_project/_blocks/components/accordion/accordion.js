897530a194be6641501a46d6296a9d399a62927c
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2020-02-13 15:58:41 +1000

    Qol 6369 accordion accessibilit y (#233)
    * QOL-6369
    Accordion accessibility issue (keyboard interaction)
    
    * QOL-6433 Quick exit fix for IE11 and QOL-6369 Accordion accessibility issue (keyboard interaction)

--- src/assets/_project/_blocks/components/accordion/accordion.js
+++ src/assets/_project/_blocks/components/accordion/accordion.js
@@ -8,9 +8,23 @@
 (function ($) {
   let accordion = '.qg-accordion';
   if ($(accordion).length > 0) {
-    let tabindex = 1;
     let accordionControls = 'input[name=control]';
+    let accItem = $(accordion).find('article');
     let linkedpanel =  window.location.hash && $('input[aria-controls=' + window.location.hash.substring(1) + ']');
+
+    // keyboard accessibility
+    var a11yClick = function (event) {
+      if (event.type === 'click') {
+        return true;
+      } else if (event.type === 'keypress') {
+        var code = event.charCode || event.keyCode;
+        if ((code === 32) || (code === 13)) {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    };
 
     //Handle events of accordion inputs
     $(accordion).find('article input').on('change', function () {
@@ -20,21 +34,6 @@
         .attr('aria-expanded', checkedStatus) //sets aria
         .parents(accordion).find(accordionControls).prop('checked', false); //clears expand/collapse selection
       controlledPanedId.attr('aria-hidden', !checkedStatus);
-    });
-
-    //expand all click
-    // label selector is to provide backward compatibility in case projects are using old markup
-    $('.qg-acc-controls .expand, label[for=\'expand\']').click(function (e) {
-      e.preventDefault();
-      $(this).focus();
-      $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', true);
-    });
-
-    // collapse all click
-    // label selector is to provide backward compatibility in case projects are using old markup
-    $('.qg-acc-controls .collapse, label[for=\'collapse\']').click(function (e) {
-      e.preventDefault();
-      $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', false);
     });
 
     // open on page load
@@ -51,25 +50,55 @@
     hashTrigger();
     window.onhashchange = hashTrigger;
 
-    // inserting tab index dynamically
-    // label selector is to provide backward compatibility in case projects are using old markup
-    $('.qg-accordion .acc-heading, .qg-acc-controls .expand, .qg-acc-controls .collapse, label[for="expand"], label[for="collapse"]').each(function () {
-      if (this.type !== 'hidden') {
-        var $input = $(this);
-        $input.attr('tabindex', tabindex);
-        tabindex++;
-      }
-    });
     $('input[name=tabs]').click(function () {
       $(this).parent('article').find('.acc-heading').focus();
     });
 
     // highlight title on hover
-    $('.qg-accordion article').hover(function () {
+    accItem.hover(function () {
       $(accordion).find('.title').removeClass('ht');
       $(this).find('.title').addClass('ht');
     }, function () {
       $(accordion).find('.title').removeClass('ht');
+    });
+
+    // expand/collapse on enter keypress
+    accItem.on('keypress', function (event) {
+      event.preventDefault();
+      if (a11yClick(event) === true) {
+        if ($(this).find('input[name="tabs"]:checked').length > 0) {
+          $(this).find('input[name="tabs"]').prop('checked', false);
+        } else {
+          $(this).find('input[name="tabs"]').prop('checked', true);
+        }
+      }
+    });
+    accItem.on('click', function (event) {
+      if (event.clientX !== 0) {
+        if ($(this).find('input[name="tabs"]:checked').length > 0) {
+          $(this).find('input[name="tabs"]').prop('checked', false);
+        } else {
+          $(this).find('input[name="tabs"]').prop('checked', true);
+        }
+        return false;
+      }
+    });
+    //expand all click
+    // label selector is to provide backward compatibility in case projects are using old markup
+    $('.qg-acc-controls .expand, label[for=\'expand\']').on('click keypress', function (event) {
+      if (a11yClick(event) === true) {
+        $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', true);
+        event.preventDefault();
+      }
+    });
+
+    // collapse all click
+    // label selector is to provide backward compatibility in case projects are using old markup
+    $('.qg-acc-controls .collapse, label[for=\'collapse\']').on('click keypress', function (event) {
+      if (a11yClick(event) === true) {
+        $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', false);
+        event.preventDefault();
+      }
     });
   }
 }(jQuery));
