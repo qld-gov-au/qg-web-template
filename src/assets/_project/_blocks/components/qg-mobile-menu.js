7c6681e4c96db3a59a1a81386a99fc8b31acfcb6
Arisa Furumoto <afurumoto@squiz.net>
2020-02-14 17:11:03 +1000

    Rearranged the markups

--- src/assets/_project/_blocks/components/qg-mobile-menu.js
+++ src/assets/_project/_blocks/components/qg-mobile-menu.js
@@ -4,9 +4,9 @@ $(function () {
     // Exception for Mobile Menu as Bootstrap collapse doesn't work smoothly for this
     $('.qg-show-menu, .qg-show-search').on('click', function () {
         if ($(this).attr('aria-expanded') == "false") {
-            $('#qg-nav-wrap').addClass('active');
+            $('#qg-site-header').addClass('active');
         } else {
-            $('#qg-nav-wrap').removeClass('active');
+            $('#qg-site-header').removeClass('active');
         }
         if ($(this).hasClass('qg-show-menu')) {
             $('#qg-search-form').collapse('hide');
