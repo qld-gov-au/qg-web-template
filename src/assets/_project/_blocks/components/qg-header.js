5d481b5c4567e425e9a5fe0602844e2b99b19161
Arisa Furumoto <afurumoto@squiz.net>
2020-02-17 11:46:05 +1000

    Added polyfill for position: sticky

--- src/assets/_project/_blocks/components/qg-header.js
+++ src/assets/_project/_blocks/components/qg-header.js
@@ -1,9 +1,15 @@
 $(function () {
     'use strict';
 
-    // Exception for Mobile Menu as Bootstrap collapse doesn't work smoothly for this
+    // Polyfill for position: sticky;
+    var Stickyfill = require('stickyfill');
+    var stickyfill = Stickyfill();
+    var elements = $('.sticky');
+    stickyfill.add(elements);
+
+    // Mobile menu & Search events to prevent both of them opening at the same time
     $('.qg-show-menu, .qg-show-search').on('click', function () {
-        if ($(this).attr('aria-expanded') == "false") {
+        if ($(this).attr('aria-expanded') === 'false') {
             $('#qg-site-header').addClass('active');
         } else {
             $('#qg-site-header').removeClass('active');
