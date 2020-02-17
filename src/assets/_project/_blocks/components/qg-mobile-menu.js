d549bed0513c1628460718f6842eb51246a6c6aa
Oliver Neal <oneal@squiz.net>
2020-02-17 11:44:01 +1000

    Fixing linting error

--- src/assets/_project/_blocks/components/qg-mobile-menu.js
+++ src/assets/_project/_blocks/components/qg-mobile-menu.js
@@ -3,7 +3,7 @@ $(function () {
 
     // Exception for Mobile Menu as Bootstrap collapse doesn't work smoothly for this
     $('.qg-show-menu, .qg-show-search').on('click', function () {
-        if ($(this).attr('aria-expanded') == "false") {
+        if ($(this).attr('aria-expanded') === 'false') {
             $('#qg-site-header').addClass('active');
         } else {
             $('#qg-site-header').removeClass('active');
