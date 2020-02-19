60ed6a55252a799efbe8ba0d13e05427b94b13f1
Oliver Neal <oneal@squiz.net>
2020-02-19 10:33:39 +1000

    release

--- release/template-local/assets/v3.1/latest/lib/ext/bootstrap.js
+++ release/template-local/assets/v3.1/latest/lib/ext/bootstrap.js
@@ -1,4 +1,4 @@
-/*! SWE 3.1.8 2020021T1332 */
+/*! SWE 3.1.8 2020023T1033 */
 !function(e){function t(i){if(n[i])return n[i].exports;var r=n[i]={i:i,l:!1,exports:{}};return e[i].call(r.exports,r,r.exports,t),r.l=!0,r.exports}var n={};t.m=e,t.c=n,t.i=function(e){return e},t.d=function(e,n,i){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:i})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=2)}([function(e,t,n){var i,r;/*!
  * jQuery JavaScript Library v3.4.1
  * https://jquery.com/
