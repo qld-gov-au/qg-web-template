48662b814e3626b3bcfd510eb670befcd41f0369
Arisa Furumoto <afurumoto@squiz.net>
2020-02-21 09:06:50 +1000

    Google Analytics tracking code

--- src/assets/_project/_blocks/layout/breadcrumbs/breadcrumbs.js
+++ src/assets/_project/_blocks/layout/breadcrumbs/breadcrumbs.js
@@ -4,11 +4,14 @@
         if (breadcrumbLength > 4) {
             for (var i = 0; i < breadcrumbLength; i++ ) {
                 if (i > 1 && i < breadcrumbLength - 2) {
-                    $(this).find('.qg-breadcrumb-list-item').eq(i).addClass('shortened');
+                    $(this).find('.qg-breadcrumb-list-item').eq(i).addClass('shortened')
+                    $(this).find('.qg-breadcrumb-list-item').eq(i).find('.qg-breadcrumb-list-item__link').attr('data-analytics-link-group','qg-breadcrumb-revealed-link');
+                } else {
+                    $(this).find('.qg-breadcrumb-list-item').eq(i).find('.qg-breadcrumb-list-item__link').attr('data-analytics-link-group','qg-breadcrumb-link');
                 }
             }
             
-            $(this).find('.qg-breadcrumb-list-item').eq(1).after("<li class='qg-breadcrumb-list-item shorten'><button class='qg-breadcrumb-toggle' aria-label='Expand the breadcrumbs'>[...]</button></li>");
+            $(this).find('.qg-breadcrumb-list-item').eq(1).after("<li class='qg-breadcrumb-list-item shorten'><button class='qg-breadcrumb-toggle' aria-label='Expand the breadcrumbs' data-analytics-link-group='qg-breadcrumb-ellipsis'>[...]</button></li>");
         }
 
         $('.qg-breadcrumb-toggle').on('click', function() {
