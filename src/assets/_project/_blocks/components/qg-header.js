5d716267aa500e61e9caa939525d1154ff4beea5
Arisa Furumoto <afurumoto@squiz.net>
2020-02-21 15:16:39 +1000

    Header tidy up

--- src/assets/_project/_blocks/components/qg-header.js
+++ src/assets/_project/_blocks/components/qg-header.js
@@ -4,15 +4,14 @@ $(function () {
     // Polyfill for position: sticky;
     var Stickyfill = require('stickyfill');
     var stickyfill = Stickyfill();
-    var elements = $('.sticky');
-    stickyfill.add(elements);
+    stickyfill.add($('.sticky')[0]);
 
     // Mobile menu & Search events to prevent both of them opening at the same time
     $('.qg-show-menu, .qg-show-search').on('click', function () {
         if ($(this).attr('aria-expanded') === 'false') {
-            $('.qg-site-header').addClass('active');
+            $('body').addClass('header-active');
         } else {
-            $('.qg-site-header').removeClass('active');
+            $('body').removeClass('header-active');
         }
         if ($(this).hasClass('qg-show-menu')) {
             $('.qg-search-form').collapse('hide');
