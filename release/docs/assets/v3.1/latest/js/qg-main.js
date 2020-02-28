1482f5525bd620f8e2f3d058a76dfbfbac055c34
Arisa Furumoto <afurumoto@squiz.net>
2020-02-28 16:20:25 +1000

    release

--- release/docs/assets/v3.1/latest/js/qg-main.js
+++ release/docs/assets/v3.1/latest/js/qg-main.js
@@ -4122,8 +4122,36 @@
 /***/ (function(module, exports) {
 
 	'use strict'; /**
-	              * Adds page and user details to hidden inputs on the feedback form
+	              * AJAX feedback form submission
 	              **/
+	
+	$('#qg-page-feedback-form').submit(function (event) {
+	  event.preventDefault();
+	  var postUrl = $(this).attr('action');
+	  var requestMethod = $(this).attr('method');
+	  var formData = new FormData(this);
+	  $.ajax({
+	    url: postUrl,
+	    type: requestMethod,
+	    data: formData,
+	    contentType: false,
+	    cache: false,
+	    processData: false }).
+	  done(function (response) {
+	    $('.qg-footer-feedback .thankyou').removeClass('d-none');
+	    $('#qg-page-feedback-form, .qg-feedback-toggle').addClass('d-none');
+	    var thankyou = $.parseHTML(response);
+	    $('.qg-footer-feedback .thankyou').append(thankyou);
+	  });
+	});
+	
+	$('.qg-footer-feedback__close').click(function () {
+	  $('#qg-page-feedback-form, .qg-feedback-toggle').removeClass('d-none');
+	});
+	
+	/**
+	    * Adds page and user details to hidden inputs on the feedback form
+	    **/
 	
 	function sanitize(str) {
 	  if (!str) {
