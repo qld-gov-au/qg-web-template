619015bcb1c08ac5647554de4e5d90a2b7cee9a5
Arisa Furumoto <afurumoto@squiz.net>
2020-02-28 13:37:34 +1000

    release

--- release/docs/assets/v3.1/latest/js/qg-main.js
+++ release/docs/assets/v3.1/latest/js/qg-main.js
@@ -2318,6 +2318,24 @@
 	            $('.qg-navigation').collapse('hide');
 	        }
 	    });
+	
+	    // Keep location dropdown open the elements inside of the dropdown are clicked
+	    $('.header-location .dropdown-menu').click(function (e) {
+	        e.stopPropagation();
+	    });
+	
+	    // Except for these button
+	    $('.clear-location, .detect-location, .set-location').click(function (e) {
+	        $('.header-location').dropdown('toggle');
+	    });
+	
+	    $('.header-location-close').click(function (e) {
+	        $('.header-location .dropdown-menu').addClass('closed');
+	        $('.header-location').dropdown('toggle');
+	        setTimeout(function () {
+	            $('.header-location .dropdown-menu').removeClass('closed');
+	        }, 300);
+	    });
 	});
 
 /***/ }),
