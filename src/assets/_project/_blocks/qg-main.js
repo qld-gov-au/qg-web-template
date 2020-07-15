9ccfebe5b5a621ab60cdb903a2f3d8dac62d69a9
asifamingov <asif.amin@dsiti.qld.gov.au>
2020-07-16 09:52:40 +1000

    added share link import

--- src/assets/_project/_blocks/qg-main.js
+++ src/assets/_project/_blocks/qg-main.js
@@ -14,7 +14,7 @@ import './components/qg-components';
 import './layout/footer/footer-legals';
 import activeSideNav      from './layout/section-nav/section-nav';
 import stepNav      from './layout/section-nav/step-nav';
-import shareLinks         from './layout/content/share-links';
+import './layout/content/share-links';
 import './layout/breadcrumbs/breadcrumbs';
 import './layout/content/content';
 import './layout/content/content-types/figure-credits-toggle';
@@ -26,6 +26,5 @@ import feedbackForm       from './layout/footer/feedback-form';
   activeSideNav.highlightNavItem();
   stepNav.init();
   feedbackForm.init(franchiseTitle);
-  shareLinks.init();
 }());
 
