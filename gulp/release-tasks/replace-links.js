39679f0dd96b801d9b373cc54474f93fb294c7d3
asifamingov <asif.amin@dsiti.qld.gov.au>
2020-08-19 09:01:42 +1000

    QOL-7081 Card component additional features

--- gulp/release-tasks/replace-links.js
+++ gulp/release-tasks/replace-links.js
@@ -3,11 +3,11 @@
 module.exports = function (gulp, plugins, es, config) {
   return function (done) {
     gulp.src(`${config.basepath.release}/docs/*.html`)
-      .pipe(plugins.replace(/\/assets\/v4\//g, 'https://static.qgov.net.au/assets/v4/'))
+      .pipe(plugins.replace(/\/assets\/v4\//g, 'https://test-static.qgov.net.au/assets/v4/'))
       .pipe(gulp.dest(`${config.basepath.release}/docs/`));
 
     gulp.src(`${config.basepath.release}/docs/pagemodels/*.html`)
-      .pipe(plugins.replace(/\/assets\/v4\//g, 'https://static.qgov.net.au/assets/v4/'))
+      .pipe(plugins.replace(/\/assets\/v4\//g, 'https://test-static.qgov.net.au/assets/v4/'))
       .pipe(gulp.dest(`${config.basepath.release}/docs/pagemodels/`));
 
     done();
