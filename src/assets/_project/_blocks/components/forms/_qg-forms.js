1cbc38428728981bf17facc57ab1f474d1a6fe09
asifamingov <asif.amin@dsiti.qld.gov.au>
2020-11-02 17:26:41 +1000

    updated custom radio button and checkbox js

--- src/assets/_project/_blocks/components/forms/_qg-forms.js
+++ src/assets/_project/_blocks/components/forms/_qg-forms.js
@@ -1,9 +1,6 @@
 (function ($) {
   'use strict';
   var $rcTheme = $('.rc-theme');
-  var $fr = $('input[type="radio"]');
-  var $fc = $('input[type="checkbox"]');
-
   function toggleFocus (e) {
     if (e.type === 'focus') {
       $(this).parents('li').addClass('rc-theme__focus');
@@ -11,22 +8,27 @@
       $(this).parents('li').removeClass('rc-theme__focus');
     }
   }
-  $rcTheme.find($fr).change(function () {
-    if ($(this).is(':checked')) {
-      $(this).parents('.rc-theme').find('li').removeClass('rc-theme__active');
-      $(this).parents('li').addClass('rc-theme__active');
-    } else {
-      $(this).parents('li').removeClass('rc-theme__active');
-    }
-  });
-  $rcTheme.find($fc).change(function () {
-    if ($(this).is(':checked')) {
-      $(this).parents('li').addClass('rc-theme__active');
-    } else {
-      $(this).parents('li').removeClass('rc-theme__active');
-    }
-  });
+  if ($rcTheme.length > 0) {
+    var $fr = $('input[type="radio"]');
+    var $fc = $('input[type="checkbox"]');
 
-  $rcTheme.find($fc).on('focus blur', toggleFocus);
-  $rcTheme.find($fr).on('focus blur', toggleFocus);
+    $rcTheme.find($fr).change(function () {
+      if ($(this).is(':checked')) {
+        $(this).parents('.rc-theme').find('li').removeClass('rc-theme__active');
+        $(this).parents('li').addClass('rc-theme__active');
+      } else {
+        $(this).parents('li').removeClass('rc-theme__active');
+      }
+    });
+    $rcTheme.find($fc).change(function () {
+      if ($(this).is(':checked')) {
+        $(this).parents('li').addClass('rc-theme__active');
+      } else {
+        $(this).parents('li').removeClass('rc-theme__active');
+      }
+    });
+
+    $rcTheme.find($fc).on('focus blur', toggleFocus);
+    $rcTheme.find($fr).on('focus blur', toggleFocus);
+  }
 }(jQuery));
