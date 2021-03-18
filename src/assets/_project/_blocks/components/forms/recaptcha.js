52c79fd5afc26053d5ff93e7fd3b12aacf2e54ab
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-03-18 12:34:01 +1000

    QOL-7763 SWE4: Bug: Recaptcha should not be enabled on all pages until a form requests it

--- src/assets/_project/_blocks/components/forms/recaptcha.js
+++ src/assets/_project/_blocks/components/forms/recaptcha.js
@@ -9,215 +9,142 @@
 import keys from '../../data/qg-google-keys';
 
 (function ($, swe) {
-  // let checkEnv = window.location.hostname.search(/\bdev\b|test|localhost|github|\buat\b/);
-  /*
-   * isProd function checks if keywords 'dev|test|localhost|github|uat' are not in the hostname then the environment is PROD.
-   * This may not work in all the cases.
-  */
-  let isProd = function () {
-    return window.location.hostname.search(/dev|test|localhost|github|\buat\b/) === -1;
-  };
-  let $feedbackForm = $('#qg-page-feedback-form');
-
-  if ($feedbackForm.length > 0) {
-    let setUrlEnableCaptcha = () => {
-      // if environment is not PROD then use test submission handler link
-      if (!isProd()) {
-        var testUrl = $feedbackForm.attr('action').replace('www.smartservice.qld.gov.au', 'test.smartservice.qld.gov.au');
-        $feedbackForm.attr('action', testUrl);
+  'use strict';
+  /**
+   * Google recaptcha
+   **/
+  var qgRecaptcha = {
+    config: {
+      $feedbackForm: $('#qg-page-feedback-form'),
+      $recaptchaOnPage: $('form[data-recaptcha="true"]'),
+    },
+    googleRecaptchaApiKey: function() {
+      return this.isProd() ? keys.defGoogleRecaptcha.prod : keys.defGoogleRecaptcha.uat;
+    },
+    footerFeedbackGoogleRecaptchaApiKey: function() {
+      return this.isProd() ? keys.defFeedbackGoogleRecaptcha.prod : keys.defFeedbackGoogleRecaptcha.uat;
+    },
+    isProd: function() {
+      return window.location.hostname.search(/dev|test|localhost|github|\buat\b/) === -1;
+    },
+    init: function() {
+      let $feedbackForm = this.config.$feedbackForm;
+      if ($feedbackForm.length > 0) {
+        if (!this.isProd()) {
+          var testUrl = $feedbackForm.attr('action').replace('www.smartservice.qld.gov.au', 'test.smartservice.qld.gov.au');
+          $feedbackForm.attr('action', testUrl);
+        }
+        // if data-recaptcha attribute is not present then insert it
+        if ($feedbackForm.attr('data-recaptcha') === undefined) {
+          $feedbackForm.attr('data-recaptcha', 'true');
+        }
+        this.footerFeedbackRecaptcha();
       }
-      // if data-recaptcha attribute is not present then insert it
-      if ($feedbackForm.attr('data-recaptcha') === undefined) {
-        $feedbackForm.attr('data-recaptcha', 'true');
-      }
-    };
-    setUrlEnableCaptcha();
-  }
-
-  let googleRecaptchaApiKey = isProd()
-    ? keys.defGoogleRecaptcha.prod
-    : keys.defGoogleRecaptcha.uat;//This is a v2 key
-  let footerFeedbackGoogleRecaptchaApiKey = isProd()
-    ? keys.defFeedbackGoogleRecaptcha.prod
-    : keys.defFeedbackGoogleRecaptcha.uat;//This is a v3 key
-
-  let ajaxMethod = function () {
-    swe.ajaxCall(
-      'https://www.google.com/recaptcha/api.js?render=' + footerFeedbackGoogleRecaptchaApiKey,
-      'script',
-      onloadRecaptchaAjax,
-      'Recaptcha unavailable',
-    );
-  };
-
-  //v3 Captcha, can have multiples
-  let v3Captcha = (form, greptcha, key, action) => {
-    //console.log('v3 key: ' + key);
-    try {
-      grecaptcha.execute(key, {action: action})
-        .then(function (token) {
-          if (greptcha.length > 0) {
+    },
+    // recaptcha version 2
+    v2Captcha: (form, subBtn, key) => {
+      try {
+        //console.log('v2 key: ' + key);
+        grecaptcha.render(subBtn, {
+          sitekey: key,
+          callback: function() {
+            var response = grecaptcha.getResponse();
             if (
-              greptcha.attr('value') !== '' ||
-              greptcha.attr('value').length !== 0 ||
-              greptcha.attr('value') !== undefined) {
-              greptcha.val(token);
+              response === '' ||
+              response === undefined ||
+              response.length === 0
+            ) {
+              console.log('Invalid recaptcha');
+              return false;
+            } else {
               form.submit();
-              return true;
             }
-          }
-          return false;
+          },
         });
-    } catch (e) {
-      return false;
-    }
-  };
-
-  //v2 Captcha, usually is singular
-  let v2Captcha = (form, subBtn, key) => {
-    try {
-      //console.log('v2 key: ' + key);
-      grecaptcha.render(subBtn, {
-        sitekey: key,
-        callback: () => {
-          var response = grecaptcha.getResponse();
-          if (
-            response === '' ||
-            response === undefined ||
-            response.length === 0
-          ) {
-            console.log('Invalid recaptcha');
-            return false;
-          } else {
-            form.submit();
-          }
-        },
-      });
-    } catch (e) {
-      grecaptcha.reset();
-      return false;
-    }
-    grecaptcha.execute();
-  };
-
-  let loadedRecaptcha = false;
-  let onloadRecaptchaAjax = () => {
-    grecaptcha.ready(function () {
-      $('#qg-page-feedback-form').submit(function (event) {
-        var self = $(this);
-        event.preventDefault();
-        if ($('#qg-page-feedback-form li.invalid').length <= 0) {
-          var greptcha = $feedbackForm.find('input[name="g-recaptcha-response"]');
-          var postUrl = self.attr('action');
-          var requestMethod = self.attr('method');
-          var $successMsgContainer = $('.thankyou');
-          grecaptcha.execute(footerFeedbackGoogleRecaptchaApiKey, {action: 'feedback'})
-            .then(function (token) {
-              if (greptcha.length > 0) {
+      } catch (e) {
+        grecaptcha.reset();
+        return false;
+      }
+      grecaptcha.execute();
+    },
+    // recaptcha version 3
+    v3Captcha: (form, greptcha, key, action) => {
+      try {
+        grecaptcha.execute(key, {action: action})
+          .then(function (token) {
+            if (greptcha.length > 0) {
+              if (
+                greptcha.attr('value') !== '' ||
+                greptcha.attr('value').length !== 0 ||
+                greptcha.attr('value') !== undefined) {
                 greptcha.val(token);
-                var formData = self.serialize();
-                $.ajax({
-                  url: postUrl,
-                  type: requestMethod,
-                  data: formData,
-                  contentType: 'application/x-www-form-urlencoded',
-                  cache: false,
-                  processData: false,
-                }).done(function (response) {
-                  $successMsgContainer.removeClass('d-none');
-                  $('#qg-page-feedback-form, .qg-feedback-toggle').addClass('d-none');
-                  $successMsgContainer.append($.parseHTML(JSON.parse(response).message));
-                }).fail(function () {
-                  $successMsgContainer.append('<p>Request failed, please try again</p>');
-                });
+                form.submit();
                 return true;
               }
-            });
-        }
-      });
-    });
-  };
-  let onloadRecaptcha = () => {
-    grecaptcha.ready(function () {
-      // eslint-disable-line
-      //v2 Forms
-      if (!loadedRecaptcha) {
-        $('form[data-recaptcha="true"]')
-          .find('input[type="submit"], button[type="submit"]')
-          .on('click', e => {
-            e.preventDefault();
-            let subBtn = e.target;
-            let form = $(subBtn).parents('form');
-            var greptcha = form.find('input[name="g-recaptcha-response"]');
-            let manualSitekey = form.attr('data-sitekey');
-            let manualAction = form.attr('data-action');
-            if (form.attr('id') === 'qg-page-feedback-form') { //Footer feedback
-              v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, 'feedback');
-            } else if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
-              v3Captcha(form, greptcha, manualSitekey, manualAction);
-            } else if (manualAction !== undefined) { //v3 manual with feedback key but differnt action
-              v3Captcha(form, greptcha, footerFeedbackGoogleRecaptchaApiKey, manualAction);
-            } else if (manualSitekey !== undefined && manualAction === undefined) { //v2 manual (no action in v2)
-              v2Captcha(form, subBtn, manualSitekey);
-            } else { //default v2 with default key
-              v2Captcha(form, subBtn, googleRecaptchaApiKey);
             }
+            return false;
           });
-        loadedRecaptcha = true;
+      } catch (e) {
+        return false;
       }
-    });
-  };
-  //https://github.com/google/recaptcha/issues/279
-  //https://github.com/google/recaptcha/issues/281
-  //https://www.hackviking.com/development/multiple-recaptcha-on-the-same-page/
-  //Setup recaptcha if on the page
-  let loadFooter = false;
-  if ($('form[data-recaptcha="true"]').length > 0) {
-    //enable recaptcha on form submits, load latest v3 version of recaptcha
-    let v2Loaded = false;
-    $('form[data-recaptcha="true"]').each(function () {
-      let manualSitekey = $(this).attr('data-sitekey');
-      let manualAction = $(this).attr('data-action');
-      if ($(this).attr('id') === 'qg-page-feedback-form') { //Footer feedback
-        //Only load if the feedback button is clicked
-        loadFooter = true;
-      } else if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
+    },
+    footerFeedbackRecaptcha: function() {
+      var selfObj = this;
+      /**
+       * call recaptcha api to load script with the key on .qg-feedback-toggle click
+       **/
+      $('.qg-feedback-toggle').one('click', function(){
         swe.ajaxCall(
-          'https://www.google.com/recaptcha/api.js?render=' + manualSitekey,
+          'https://www.google.com/recaptcha/api.js?render=' + selfObj.footerFeedbackGoogleRecaptchaApiKey(),
           'script',
-          onloadRecaptcha,
+          onReady,
           'Recaptcha unavailable',
         );
-      } else {
-        if (!v2Loaded) {
-          swe.ajaxCall(
-            'https://www.google.com/recaptcha/api.js',
-            'script',
-            onloadRecaptcha,
-            'Recaptcha unavailable',
-          );
-          v2Loaded = true;
-        }
-      }
-    });
-    //As v3 key is used in footer and could also be used on the page with a differnt action, we need to ensure we only load it once
-    if (loadFooter) {
-      //Only load if the feedback button is clicked
-      $('.qg-feedback-toggle').one('click', ajaxMethod);
-      swe.ajaxCall(
-        'https://www.google.com/recaptcha/api.js?render=' + footerFeedbackGoogleRecaptchaApiKey,
-        'script',
-        onloadRecaptcha,
-        'Recaptcha unavailable',
-      );
-    }
-    //If all forms have captchaPrivacyTerms, we can hide reCAPTCHA Badge
-    if ($('p[class="captchaPrivacyTerms"]').length === $('form[data-recaptcha="true"]').length) {
-      var hidegrecaptchaBadge = '.grecaptcha-badge { visibility: hidden; }';
-      var styleSheet = document.createElement('style');
-      styleSheet.type = 'text/css';
-      styleSheet.innerText = hidegrecaptchaBadge;
-      document.head.appendChild(styleSheet);
-    }
-  }
+      });
+      /**
+       * @return {undefined}
+       * On ready function is executed after calling recaptcha api in the above code
+       * This function creates a submit event
+       * Call submission handler api
+       * Get and display success message
+       **/
+      let onReady = function (){
+        grecaptcha.ready(function () {
+          $('#qg-page-feedback-form').submit(function (event) {
+            var self = $(this);
+            event.preventDefault();
+            if ($('#qg-page-feedback-form li.invalid').length <= 0) {
+              var greptcha = selfObj.config.$feedbackForm.find('input[name="g-recaptcha-response"]');
+              var postUrl = self.attr('action');
+              var requestMethod = self.attr('method');
+              var $successMsgContainer = $('.thankyou');
+              grecaptcha.execute(selfObj.footerFeedbackGoogleRecaptchaApiKey(), {action: 'feedback'})
+                .then(function (token) {
+                  if (greptcha.length > 0) {
+                    greptcha.val(token);
+                    var formData = self.serialize();
+                    $.ajax({
+                      url: postUrl,
+                      type: requestMethod,
+                      data: formData,
+                      contentType: 'application/x-www-form-urlencoded',
+                      cache: false,
+                      processData: false,
+                    }).done(function (response) {
+                      $successMsgContainer.removeClass('d-none');
+                      $('#qg-page-feedback-form, .qg-feedback-toggle').addClass('d-none');
+                      $successMsgContainer.append($.parseHTML(JSON.parse(response).message));
+                    }).fail(function () {
+                      $successMsgContainer.append('<p>Request failed, please try again</p>');
+                    });
+                    return true;
+                  }
+                });
+            }
+          });
+        });
+      };
+    },
+  };
+  qgRecaptcha.init();
 })(jQuery, qg.swe);
