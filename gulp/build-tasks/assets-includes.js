24489989f330ed1a9f82674718778505fe59648f
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-08-17 11:00:21 +1000

    version locked

--- gulp/build-tasks/assets-includes.js
+++ gulp/build-tasks/assets-includes.js
@@ -1,4 +1,5 @@
 'use strict';
+const pjson = require('../../package.json');
 
 module.exports = function (gulp, plugins, config, dest, local = false, relpath = false) {
   return function (cb) {
@@ -6,6 +7,10 @@ module.exports = function (gulp, plugins, config, dest, local = false, relpath =
       `${config.basepath.src}/assets/_project/_blocks/layout/**/*.html`,
     ];
       //.concat(config.build.excludes); //remove concat excludes, remove from gulp-config.json also
+    let versionLocked = {
+      regex: new RegExp(`/assets/${config.versionName}/{{version}}/`, 'g'),
+      replacement: `/assets/${config.versionName}/${pjson.version}/`,
+    };
     let cdnLink = {
       regex: new RegExp(`="/assets/${config.versionName}`, 'g'),
       replacement: `="https://static.qgov.net.au/assets/${config.versionName}`,
@@ -14,20 +19,16 @@ module.exports = function (gulp, plugins, config, dest, local = false, relpath =
       regex: new RegExp(`="/assets/includes-local`, 'g'),
       replacement: `="/assets/includes-cdn`,
     };
-
-    // if (!Array.isArray(dest)) {
-    //   dest = [dest];
-    // }
-
-    const projectAssets = new RegExp('="(/)?assets/_project/', 'g');
+    // const projectAssets = new RegExp('="(/)?assets/_project/', 'g');
 
     // Test if the element is set to deploy this component
     return gulp.src(src, { dot: true })
       .pipe(plugins.include({ hardFail: true }))
       .on('error', console.log)
-      .pipe(plugins.replace(projectAssets, `="$1assets/${config.versionName}/latest/`)) // Replace '_project' with 'v3'
+      // .pipe(plugins.replace(projectAssets, `="$1assets/${config.versionName}/latest/`)) // Replace '_project' with 'v3'
       .pipe(plugins.if(local !== true, plugins.replace(cdnLink.regex, cdnLink.replacement)))
       .pipe(plugins.if(local !== true, plugins.replace(folderNameChange.regex, folderNameChange.replacement)))
+      .pipe(plugins.replace(versionLocked.regex, versionLocked.replacement))
       .pipe(gulp.dest(`${config.basepath.build}/${dest}/`));
   };
 };
