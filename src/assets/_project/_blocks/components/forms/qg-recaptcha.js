5a6daa05da036935ab702aa957da1efa67bedce7
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-09-07 09:39:47 +1000

    QOL-8220 SWE to allow multiple recaptcha keys (form.io conflict)

--- src/assets/_project/_blocks/components/forms/qg-recaptcha.js
+++ src/assets/_project/_blocks/components/forms/qg-recaptcha.js
@@ -32,12 +32,10 @@ import keys from '../../data/qg-google-keys';
         if ($feedbackForm.attr('data-recaptcha') === undefined) {
           $feedbackForm.attr('data-recaptcha', 'true');
         }
+        // load google recaptcha lib
+        this.loadRecaptchaLib();
         // If all forms have captchaPrivacyTerms, we can hide reCAPTCHA Badge
         this.hideCaptchaBanner();
-        // footerFeedbackRecaptcha supports global footer feedback ajax based submissions.
-        this.footerFeedbackRecaptcha();
-        //for Backward compatibility legacyRecaptcha supports both version v2 and v3 for non ajax based submissions.
-        this.legacyRecaptcha();
       }
     },
 
@@ -66,66 +64,49 @@ import keys from '../../data/qg-google-keys';
     },
 
     /**
-     * From SWE4 onwards footer feedback is AJAX based
+     * from swe4 onwards footer feedback is ajax based
+     * footerFeedbackRecaptcha function handles recaptcha on ajax based form.
+     * This function creates a submit event
+     * Call submission handler api
+     * Get and display success message
      * @return {undefined}
      **/
     footerFeedbackRecaptcha: function() {
-      var selfObj = this;
-      /**
-       * call recaptcha api to load script with the key on .qg-feedback-toggle click
-       **/
-      $('.qg-feedback-toggle').one('click', function(){
-        swe.ajaxCall(
-          'https://www.google.com/recaptcha/api.js?render=' + selfObj.footerFeedbackGoogleRecaptchaApiKey(),
-          'script',
-          onReady,
-          'Recaptcha unavailable',
-        );
-      });
-
-      /**
-       * onReady function is executed after calling recaptcha api in the above code
-       * This function creates a submit event
-       * Call submission handler api
-       * Get and display success message
-       * @return {undefined}
-       **/
-      let onReady = function (){
-        grecaptcha.ready(function () {
-          $('#qg-page-feedback-form').submit(function (event) {
-            var self = $(this);
-            event.preventDefault();
-            if ($('#qg-page-feedback-form li.invalid').length <= 0) {
-              var greptcha = selfObj.config.$feedbackForm.find('input[name="g-recaptcha-response"]');
-              var postUrl = self.attr('action');
-              var requestMethod = self.attr('method');
-              var $successMsgContainer = $('.thankyou');
-              grecaptcha.execute(selfObj.footerFeedbackGoogleRecaptchaApiKey(), {action: 'feedback'})
-                .then(function (token) {
-                  if (greptcha.length > 0) {
-                    greptcha.val(token);
-                    var formData = self.serialize();
-                    $.ajax({
-                      url: postUrl,
-                      type: requestMethod,
-                      data: formData,
-                      contentType: 'application/x-www-form-urlencoded',
-                      cache: false,
-                      processData: false,
-                    }).done(function (response) {
-                      $successMsgContainer.removeClass('d-none');
-                      $('#qg-page-feedback-form, .qg-feedback-toggle').addClass('d-none');
-                      $successMsgContainer.append($.parseHTML(JSON.parse(response).message));
-                    }).fail(function () {
-                      $successMsgContainer.append('<p>Request failed, please try again</p>');
-                    });
-                    return true;
-                  }
-                });
-            }
-          });
+      var self = this;
+      grecaptcha.ready(() => {
+        $('#qg-page-feedback-form').submit(function (event) {
+          var targetFormSubmit = $(this);
+          event.preventDefault();
+          if ($('#qg-page-feedback-form li.invalid').length <= 0) {
+            var greptcha = self.config.$feedbackForm.find('input[name="g-recaptcha-response"]');
+            var postUrl = targetFormSubmit.attr('action');
+            var requestMethod = targetFormSubmit.attr('method');
+            var $successMsgContainer = $('.thankyou');
+            grecaptcha.execute(self.footerFeedbackGoogleRecaptchaApiKey(), {action: 'feedback'})
+              .then(function (token) {
+                if (greptcha.length > 0) {
+                  greptcha.val(token);
+                  var formData = targetFormSubmit.serialize();
+                  $.ajax({
+                    url: postUrl,
+                    type: requestMethod,
+                    data: formData,
+                    contentType: 'application/x-www-form-urlencoded',
+                    cache: false,
+                    processData: false,
+                  }).done(function (response) {
+                    $successMsgContainer.removeClass('d-none');
+                    $('#qg-page-feedback-form, .qg-feedback-toggle').addClass('d-none');
+                    $successMsgContainer.append($.parseHTML(JSON.parse(response).message));
+                  }).fail(function () {
+                    $successMsgContainer.append('<p>Request failed, please try again</p>');
+                  });
+                  return true;
+                }
+              });
+          }
         });
-      };
+      });
     },
 
     /**
@@ -204,75 +185,80 @@ import keys from '../../data/qg-google-keys';
         return false;
       }
     },
-
     /**
-     * for Backward compatibility
-     * legacyRecaptcha supports both version v2 and v3 for non ajax based submissions.
+     * loadRecaptchaLib load google recaptcha lib based on some conditions
      *  @return {undefined}
      **/
-    legacyRecaptcha: function(){
+    loadRecaptchaLib: function (){
       let self = this;
-      let loadedRecaptcha = false;
       /**
-       * onloadRecaptcha
-       * @return {undefined}
+       * handles footer feedback recaptcha load
        **/
-      let onloadRecaptcha = () => {
-        grecaptcha.ready(function () {
-          //v2 Forms
-          if (!loadedRecaptcha) {
-            $('[data-recaptcha="true"]:not(#qg-page-feedback-form)')
-              .find('input[type="submit"], button[type="submit"]')
-              .on('click', e => {
-                e.preventDefault();
-                let subBtn = e.target;
-                let form = $(subBtn).parents('form');
-                var greptcha = form.find('input[name="g-recaptcha-response"]');
-                let manualSitekey = form.attr('data-sitekey');
-                let manualAction = form.attr('data-action');
-                if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
-                  self.v3Captcha(form, greptcha, manualSitekey, manualAction);
-                } else if (manualAction !== undefined) { //v3 manual with feedback key but differnt action
-                  self.v3Captcha(form, greptcha, self.footerFeedbackGoogleRecaptchaApiKey(), manualAction);
-                } else if (manualSitekey !== undefined) { //v2 manual (no action in v2)
-                  self.v2Captcha(form, subBtn, manualSitekey);
-                } else { //default v2 with default key
-                  self.v2Captcha(form, subBtn, self.googleRecaptchaApiKey());
-                }
+      $('.qg-feedback-toggle').one('click', function(){
+        alert('load footer recp');
+        $.getScript('https://www.google.com/recaptcha/api.js?render=' + self.footerFeedbackGoogleRecaptchaApiKey(), function (){
+          self.footerFeedbackRecaptcha();
+        });
+      });
+      /**
+       * handles all other forms
+       **/
+      setTimeout(function (){
+        if ($('#qg-primary-content').find('form[data-recaptcha="true"]').length > 0) {
+          //enable recaptcha on form submits, load latest v3 version of recaptcha
+          let v2Loaded = false;
+          $('form[data-recaptcha="true"]').each(function () {
+            let manualSitekey = $(this).attr('data-sitekey');
+            let manualAction = $(this).attr('data-action');
+            if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
+              $.getScript('https://www.google.com/recaptcha/api.js?render=' + manualSitekey, function (){
+                self.onloadRecaptcha();
               });
-            loadedRecaptcha = true;
-          }
-        });
-      };
-      /**
-       * onloadRecaptcha
-       **/
-      if ($('form[data-recaptcha="true"]').length > 0) {
-        //enable recaptcha on form submits, load latest v3 version of recaptcha
-        let v2Loaded = false;
-        $('form[data-recaptcha="true"]').each(function () {
-          let manualSitekey = $(this).attr('data-sitekey');
-          let manualAction = $(this).attr('data-action');
-          if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
-            swe.ajaxCall(
-              'https://www.google.com/recaptcha/api.js?render=' + manualSitekey,
-              'script',
-              onloadRecaptcha,
-              'Recaptcha unavailable',
-            );
-          } else {
-            if (!v2Loaded) {
-              swe.ajaxCall(
-                'https://www.google.com/recaptcha/api.js',
-                'script',
-                onloadRecaptcha,
-                'Recaptcha unavailable',
-              );
-              v2Loaded = true;
+            } else {
+              if (!v2Loaded) {
+                alert('loaded');
+                $.getScript('https://www.google.com/recaptcha/api.js', function (){
+                  self.onloadRecaptcha();
+                });
+                v2Loaded = true;
+              }
             }
-          }
-        });
-      }
+          });
+        }
+      }, 200);
+    },
+    /**
+     * onloadRecaptcha is loaded after recpatcha lib is loaded , it is passed as a callback function
+     * @return {undefined}
+     **/
+    onloadRecaptcha: function(){
+      let self = qgRecaptcha;
+      let loadedRecaptcha = false;
+      grecaptcha.ready(function () {
+        //v2 Forms
+        if (!loadedRecaptcha) {
+          $('[data-recaptcha="true"]:not(#qg-page-feedback-form)')
+            .find('input[type="submit"], button[type="submit"]')
+            .on('click', e => {
+              e.preventDefault();
+              let subBtn = e.target;
+              let form = $(subBtn).parents('form');
+              let greptcha = form.find('input[name="g-recaptcha-response"]');
+              let manualSitekey = form.attr('data-sitekey');
+              let manualAction = form.attr('data-action');
+              if (manualSitekey !== undefined && manualAction !== undefined) { //v3 manual form
+                self.v3Captcha(form, greptcha, manualSitekey, manualAction);
+              } else if (manualAction !== undefined) { //v3 manual with feedback key but differnt action
+                self.v3Captcha(form, greptcha, self.footerFeedbackGoogleRecaptchaApiKey(), manualAction);
+              } else if (manualSitekey !== undefined) { //v2 manual (no action in v2)
+                self.v2Captcha(form, subBtn, manualSitekey);
+              } else { //default v2 with default key
+                self.v2Captcha(form, subBtn, self.googleRecaptchaApiKey());
+              }
+            });
+          loadedRecaptcha = true;
+        }
+      });
     },
   };
   qgRecaptcha.init();
