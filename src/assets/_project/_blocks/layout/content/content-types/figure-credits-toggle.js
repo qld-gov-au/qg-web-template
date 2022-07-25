e7683428fc3711de2e2ec2a9f6b9d4798e955bbc
devonpis <devonpis@gmail.com>
2022-07-25 15:06:32 +1000

    update deprecation message (#477)

--- src/assets/_project/_blocks/layout/content/content-types/figure-credits-toggle.js
+++ src/assets/_project/_blocks/layout/content/content-types/figure-credits-toggle.js
@@ -19,6 +19,6 @@ $(() => {
 
   // decommission qg-cut-in warning
   if ($(figureElement).length) {
-    console.warn(`".qg-cut-in" and ".qg-cut-in-alt" is going to be deprecated in SWE library. Please replace ".qg-cut-in" or ".qg-cut-in-alt" with ".qg-fig" before 22nd Dec 2022. Please refer to https://qld-gov-au.github.io/web-template-release/components/images.html for more details.`);
+    console.warn(`".qg-cut-in" and ".qg-cut-in-alt" is going to be deprecated in SWE library. Please replace ".qg-cut-in" or ".qg-cut-in-alt" with ".qg-fig". Please refer to https://qld-gov-au.github.io/web-template-release/components/images.html for more details.`);
   }
 });
