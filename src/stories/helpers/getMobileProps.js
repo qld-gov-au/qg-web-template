44a6236f8e59fb167508ca6309f33a0186feac1a
Devon Chau <devonpis@gmail.com>
2022-09-09 17:43:50 +1000

    update mobile stories rendering

--- src/stories/helpers/getMobileProps.js
+++ src/stories/helpers/getMobileProps.js
@@ -1,9 +1,16 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 export const getCanvasMobileProps = () => ({ style: INITIAL_VIEWPORTS.iphone12.styles });
+
 export const getStoryMobileParameters = () => ({
   viewport: {
     viewports: INITIAL_VIEWPORTS,
     defaultViewport: 'iphone12',
   },
+  docs: {
+    // Opt-out of inline rendering
+    inlineStories: false,
+  },
 });
+
+export const getStoryMobileHeight = () => INITIAL_VIEWPORTS.iphone12.styles.height;
