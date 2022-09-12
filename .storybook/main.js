546a5813dbb5c3ad5d0e56fe851628df2556ef3e
devonpis <devonpis@gmail.com>
2022-09-12 16:37:01 +1000

    Qol 9230 add storybook part3 (#495)
    * add first batch of components
    
    * Qol 9230 add storybook part4 (#496)
    
    * add ssi support to storybook webpack
    
    * add mobile story helpers
    
    * add mobile story to Accordion
    
    * add template stories
    
    * add layout stories first batch
    
    * update mobile stories rendering
    
    * Add example stories tpo show states of a component (#497)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,40 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-pseudo-states",
   ],
-  "staticDirs": ['../build'],
-  "previewMainTemplate": "./.storybook/previewMainTemplate.ejs",
-  "framework": "@storybook/html"
-}
+  staticDirs: ["../build"],
+  previewMainTemplate: "./.storybook/previewMainTemplate.ejs",
+  framework: "@storybook/html",
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.html?$/,
+      use: [
+        {
+          loader: "webpack-ssi-include-loader",
+          options: {
+            localPath: "/",
+            location: "http://localhost:6006", // http url where the file can be dl
+            onFileMatch: (filePath, fileContent, isLocal) => {
+              return fileContent.replaceAll(
+                'virtual=".',
+                `virtual="${filePath.slice(0, filePath.lastIndexOf("/"))}/.`
+              );
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
