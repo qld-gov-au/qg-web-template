640d1f4acc6a06f970494051e8509fd9b997d538
devonpis <devonpis@gmail.com>
2022-09-12 16:36:35 +1000

    Add example stories tpo show states of a component (#497)

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-pseudo-states",
   ],
   staticDirs: ["../build"],
   previewMainTemplate: "./.storybook/previewMainTemplate.ejs",
