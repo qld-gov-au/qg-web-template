701b123c73c4b655a18f5020f423ef9ad740ad9d
stv-p <93984041+stv-p@users.noreply.github.com>
2023-03-20 13:05:56 +1000

    Disabled hover/pause

--- src/assets/_project/_blocks/components/carousel/qg-carousel.js
+++ src/assets/_project/_blocks/components/carousel/qg-carousel.js
@@ -32,6 +32,7 @@
   };
   $('.qg-featured .carousel.slide').each(function (i, e) {
     let carousel = $(e).attr('id');
+<<<<<<< HEAD
     carousels.push(carousel);
     $(this).attr('data-state', 'cycle');
     $('#' + carousel).carousel({
@@ -47,10 +48,38 @@
         $parentCarousel.carousel('cycle').carousel('next').attr('data-state', 'cycle');
       }
       $(this).find('span.fa, i.fa').toggleClass('fa-sync fa-pause');
+=======
+
+    //Disable hover behaviour #QOLDEV-67
+    $('#' + carousel).carousel({
+      pause: false,
+>>>>>>> 0e299731 (QOLSVC-1058 disbale pause on hover)
     });
+
+    carousels.push(carousel);
+
+    //Defaults to cycle
+    $(this).attr('data-state', 'cycle');
+
+    $('#' + carousel)
+      .find('.toggleCarousel')
+      .on('click', function (e) {
+        e.preventDefault();
+
+        //Get carousel
+        var $parentCarousel = $(this).parents('div.carousel.slide');
+
+        //Switch carousel state between cycle/pause
+        $parentCarousel.attr('data-state') === 'cycle'
+          ? $parentCarousel.attr('data-state', 'pause')
+          : $parentCarousel.attr('data-state', 'cycle');
+        $parentCarousel.carousel($parentCarousel.attr('data-state'));
+
+        $(this).find('span.fa, i.fa').toggleClass('fa-sync fa-pause');
+      });
   });
+
   window.onload = function () {
     eqHeight(carousels);
   };
 })(jQuery);
-
