f932fff874b793886f827e9be71b5cf2cd14f6a6
Ghazal Salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-07-10 15:25:51 +1000

    htm-loader options object

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,7 +24,7 @@ module.exports = {
         rule.use = [{
           loader: "html-loader",
           options: {
-            attributes: false
+            sources: false
           }
         }, {
           loader: "webpack-ssi-include-loader",
