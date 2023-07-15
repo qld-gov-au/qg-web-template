e2c3417ac0c5ce2264c6808bb0d738b3264b1c96
william dutton <will.dutt@gmail.com>
2023-07-15 15:49:50 +1000

    post storybook 7 auto migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,87 +1,56 @@
-// .storybook/main.js
-
-import path from 'path'
-import CopyPlugin from 'copy-webpack-plugin'
-
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../src/stories/Introduction.mdx", //First page to load
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(mdx|js|jsx|ts|tsx)"], // *.mdx is default, *.stories.(mdx|js|jsx|ts|ts) is V6 way)
-  addons: [
-    "@storybook/addon-links",
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "storybook-addon-pseudo-states"
-  ],
-  staticDirs: ['../build', 'storybook-static'],
-  previewMainTemplate: "./.storybook/previewMainTemplate.ejs", //see https://storybook.js.org/docs/react/addons/writing-presets for example link
+    "storybook-addon-pseudo-states",
+    "@storybook/addon-mdx-gfm",
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },],
+  staticDirs: ["../build"],
+  previewMainTemplate: "./.storybook/previewMainTemplate.ejs",
   framework: {
     name: "@storybook/html-webpack5",
     options: {}
   },
-  webpackFinal: async (config) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
     // Make whatever fine-grained changes you need
 
-    //Don't use static cdn on storybook, use internal
-    //Handle templates by referencing built assets where required so webpack can compact them and
-    //not throw errors
-    config.module.rules.unshift({
-        test: /\.html$/,
-        loader: 'string-replace-loader',
-        options: {
-          multiple: [
-            {
-              search: /https:\/\/www.qld.gov.au\/__data\/assets\/image\/\d+\/\d+\//,
-              replace: './assets/images/placeholders/',
-              flags: 'g'
-            },
-            {
-              search: 'https://www.smartservice.qld.gov.au/payment/minicart/',
-              replace: './assets/images/placeholders/',
-              flags: 'g'
-            },
-            {
-              search: 'https://static.qgov.net.au/',
-              replace: './',
-              flags: 'g'
-            },
-            {
-              search: '"/assets/',
-              replace: '"./../../build/assets/',
-              flags: 'g'
-            }
-          ]
-        }
-      })
-
     config.module.rules.forEach(rule => {
       const pattern = /html-loader/;
-      if (rule.use && pattern.test(rule.use)) { //if html-loader plugin.
-        rule.use = [
-          {
-            loader: "html-loader",
-            options: {
-              minimize: false,
-              esModule: false,
-            },
-          },
-          {
-            loader: "webpack-ssi-include-loader",
-            options: {
-              localPath: path.join(__dirname, '/build'),
-              location: "http://localhost:6006",
-              // http url where the file can be dl
-              onFileMatch: (filePath, fileContent, isLocal) => {
-                return fileContent
-                  .replaceAll('virtual=".', `virtual="${filePath.slice(0, filePath.lastIndexOf("/"))}/.`)
-                  .replaceAll('src="/assets', `src="${process.env.PUBLIC_PATH?.replace(/\/+$/, "") || ""}${process.env.ASSETS_PATH?.replace(/\/+$/, "") || ""}/assets`);
-              }
+      if (rule.use && pattern.test(rule.use)) {
+        rule.use = [{
+          loader: "html-loader",
+          options: {
+            attributes: false
+          }
+        }, {
+          loader: "webpack-ssi-include-loader",
+          options: {
+            localPath: "/",
+            location: process.env.PUBLIC_PATH?.replace(/\/+$/, "") || "http://localhost:6006",
+            // http url where the file can be dl
+            onFileMatch: (filePath, fileContent, isLocal) => {
+              return fileContent.replaceAll('virtual=".', `virtual="${filePath.slice(0, filePath.lastIndexOf("/"))}/.`).replaceAll('src="/assets', `src="${process.env.PUBLIC_PATH?.replace(/\/+$/, "") || ""}${process.env.ASSETS_PATH?.replace(/\/+$/, "") || ""}/assets`);
             }
           }
-          ];
+        }];
       }
     });
     config.plugins.push(new CopyPlugin({
@@ -98,8 +67,7 @@ const config = {
     return config;
   },
   docs: {
-    autodocs: true,
-    defaultName: 'Docs', // set to change the name of generated docs entries
+    autodocs: true
   }
 };
 export default config;
