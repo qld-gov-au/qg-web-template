dc52d35b26f7bc671a2214a695ef2732e5a402a8
Ghazal Salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-07-27 15:50:34 +1000

    Adding js in the header

--- src/assets/_project/_blocks/utils/qg-datepicker.js
+++ src/assets/_project/_blocks/utils/qg-datepicker.js
@@ -14,6 +14,7 @@ if (!browserSupportsDateInput() && $('input[type=\'date\']').length > 0) {
 if ($('input[class*=\'qg-date-input\']').length > 0) {
   $.getScript('{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.js', function () {
     $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.css' type='text/css' media='screen' />"));
+    $('head').append($("<script src='{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.js'></script>"));
     $('input[class~=\'qg-date-input\']').datepicker({
       dateFormat: 'dd/mm/yy',
       changeYear: true,
