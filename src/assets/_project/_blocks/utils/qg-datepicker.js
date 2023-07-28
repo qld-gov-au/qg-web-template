a4812bbeb1ca058515dbcab00385e35360deb27b
ghazal-salehi <126438691+ghazal-salehi@users.noreply.github.com>
2023-07-28 15:23:39 +1000

    Qoldev 422 jquery datepicker (#1035)
    * QOLDEV-422 jQuery datepicker does not need text='date'
    * Removing unsupported comment
    * removing hasDatepicker before initialising datepicker after document is ready

--- src/assets/_project/_blocks/utils/qg-datepicker.js
+++ src/assets/_project/_blocks/utils/qg-datepicker.js
@@ -14,11 +14,17 @@ if (!browserSupportsDateInput() && $('input[type=\'date\']').length > 0) {
 if ($('input[class*=\'qg-date-input\']').length > 0) {
   $.getScript('{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.js', function () {
     $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.css' type='text/css' media='screen' />"));
-    $('.qg-date-input').datepicker({
-      dateFormat: 'dd/mm/yy',
-      changeYear: true,
-      changeMonth: true,
-    });
-    $('.qg-date-input').attr('type', 'date');
   });
 }
+jQuery(document).ready(function() {
+  // hasDatepicker class has to be removed from the input when the document is ready. jquery-ui.min.js will add the
+  // calendar widget when the class does not exist on the input. Then hasDatepicker will be dynamically added to the input.
+  // This needs to be done when the document is ready
+  $('.qg-date-input').removeClass('hasDatepicker');
+  $('.qg-date-input').datepicker({
+    dateFormat: 'dd/mm/yy',
+    changeYear: true,
+    changeMonth: true,
+  });
+});
+
