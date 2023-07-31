78e7b29a2b44c946810fb57d720cc0a580598aee
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2023-07-31 14:49:56 +1000

    QOLDEV-518 wording changes to console message

--- src/assets/_project/_blocks/components/site-search/qg-funnelback-v16-refs.js
+++ src/assets/_project/_blocks/components/site-search/qg-funnelback-v16-refs.js
@@ -13,8 +13,9 @@
       newValue = newValue.replace(new RegExp(pair[0], "g"), pair[1]);
 
       if (originalValue.includes(pair[0])) {
+        let depReference = pair[1].includes('qgov~') ? `"${pair[0]}" collection,` : `"${pair[0]}"`;
         console.log(
-          `SWE/Funnelback notice:\nThis application contains a reference to "${pair[0]}" which is deprecated. It must be replaced with "${pair[1]}".\n`
+          `SWE/Funnelback notice:\nThis application contains a reference to ${depReference} which is deprecated. It must be replaced with "${pair[1]}".\n`
         );
       }
     });
