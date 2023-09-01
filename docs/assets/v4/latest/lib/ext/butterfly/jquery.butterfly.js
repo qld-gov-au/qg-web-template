06cbd6e99fe941a363810cd1260d90a4b2b5079d
ghazal-salehi <126438691+ghazal-salehi@users.noreply.github.com>
2023-09-01 01:55:39 +0000

    Deploying to gh-pages from @ qld-gov-au/qg-web-template@605d4d4bf43440b042775fea3bd90867635a840b 🚀

--- docs/assets/v4/latest/lib/ext/butterfly/jquery.butterfly.js
+++ docs/assets/v4/latest/lib/ext/butterfly/jquery.butterfly.js
@@ -81,13 +81,13 @@ jQuery.butterfly.linkCount = 0;
 /**
  * Standard key mappings
  */
-	DOM_VK_END    = 35;
-	DOM_VK_HOME   = 36;
-	DOM_VK_LEFT   = 37;
-	DOM_VK_UP     = 38;
-	DOM_VK_RIGHT  = 39;
-	DOM_VK_DOWN   = 40;
-	DOM_VK_ESCAPE = 27;
+const DOM_VK_END    = 35;
+const DOM_VK_HOME   = 36;
+const DOM_VK_LEFT   = 37;
+const DOM_VK_UP     = 38;
+const DOM_VK_RIGHT  = 39;
+const DOM_VK_DOWN   = 40;
+const DOM_VK_ESCAPE = 27;
 
 (function( $, ResizeEvents ) {// start closure
 	'use strict';
