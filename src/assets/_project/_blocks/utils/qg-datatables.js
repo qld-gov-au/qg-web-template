2a3146432ffd7412aaf31ab263a2a4c2c1a367e2
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-07 11:38:41 +1000

    QOLDEV-569 Add qg-btn after load so the buttons exist

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,1 +1,4 @@
-$('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
+$(window).on('load', function() {
+    $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
+});
+
