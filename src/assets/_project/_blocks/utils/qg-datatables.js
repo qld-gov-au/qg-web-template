082e27f6defedd7bdd0e4a9bd26c429706b7b712
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-12 13:11:05 +1000

    QOLDEV-569 searchable table improvement

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,3 +1,5 @@
-$(window).on('load', function() {
+var addQGButtonClass = function() {
     $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
-});
+};
+$(window).on('load', addQGButtonClass);
+$('.sortable-table select').on('change', addQGButtonClass);
