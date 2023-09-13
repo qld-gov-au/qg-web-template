d8892460b4355e5c043d580ebeed9813f25ecba8
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-13 19:21:47 +1000

    QOLDEV-599 Updating links to jquery ui assets

--- src/assets/_project/_blocks/utils/qg-datepicker.js
+++ src/assets/_project/_blocks/utils/qg-datepicker.js
@@ -12,8 +12,8 @@ if (!browserSupportsDateInput() && $('input[type=\'date\']').length > 0) {
 }
 // 'qg-date-input' adds a jquery ui datepicker
 if ($("input[class*='qg-date-input']").length > 0) {
-  $.getScript('{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.js', function () {
-    $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.css' type='text/css' media='screen' />"));
+  $.getScript('https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', function () {
+    $('head').append($("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css' type='text/css' media='screen' />"));
   });
 }
 
