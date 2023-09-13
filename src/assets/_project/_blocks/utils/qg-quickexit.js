b31875d39ce58b2330b55f0cd772149149ed4845
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-13 10:35:47 +1000

    QOLDEV-602 Fixing lint warnings

--- src/assets/_project/_blocks/utils/qg-quickexit.js
+++ src/assets/_project/_blocks/utils/qg-quickexit.js
@@ -1,15 +1,15 @@
-var handleQuickExit = function(e) {
-    var $el = $('.qg-quick-exit');
-    if (document.documentElement.clientWidth > 992) {
-        if ($(this).scrollTop() > 200) {
-            $el.css({'position': 'fixed', 'top': '0px'});
-        }
-        if ($(this).scrollTop() < 200) {
-            $el.css({'position': 'sticky', 'top': '0px'});
-        }
-    } else {
-        $el.css({'position': 'fixed', 'top': 'auto'});
+var handleQuickExit = function (e) {
+  var $el = $('.qg-quick-exit');
+  if (document.documentElement.clientWidth > 992) {
+    if ($(this).scrollTop() > 200) {
+      $el.css({ 'position': 'fixed', 'top': '0px' });
     }
+    if ($(this).scrollTop() < 200) {
+      $el.css({ 'position': 'sticky', 'top': '0px' });
+    }
+  } else {
+    $el.css({ 'position': 'fixed', 'top': 'auto' });
+  }
 };
 $(window).on('scroll', handleQuickExit);
 window.addEventListener('resize', handleQuickExit, true);
