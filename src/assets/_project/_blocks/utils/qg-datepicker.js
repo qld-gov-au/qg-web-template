82551c4d4d69fa9b76b674a770a0d3d8f314c3dd
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-13 15:02:24 +1000

    datepicker

--- src/assets/_project/_blocks/utils/qg-datepicker.js
+++ src/assets/_project/_blocks/utils/qg-datepicker.js
@@ -11,14 +11,14 @@ if (!browserSupportsDateInput() && $('input[type=\'date\']').length > 0) {
   });
 }
 // 'qg-date-input' adds a jquery ui datepicker
-if ($('input[class*=\'qg-date-input\']').length > 0) {
+//if ($('input[class*=\'qg-date-input\']').length > 0) {
   $.getScript('{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.js', function () {
     $('head').append($("<link rel='stylesheet' href='{{CDN}}/latest/lib/ext/jquery-ui-bundle/jquery-ui.min.css' type='text/css' media='screen' />"));
   });
-}
+//}
 
-document.addEventListener('DOMContentLoaded', function(event){
-//$(window).on('load', function() {
+//document.addEventListener('DOMContentLoaded', function(event){
+$(window).on('load', function() {
   if ($('input[class*=\'qg-date-input\']').length > 0) {
     // hasDatepicker class has to be removed from the input when the page is loaded. jquery-ui.min.js will add the
     // calendar widget when the class does not exist on the input. Then hasDatepicker will be dynamically added to the input.
