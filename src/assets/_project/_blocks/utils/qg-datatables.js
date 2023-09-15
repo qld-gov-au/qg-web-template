f3781cfce95be7d00ac6fd7b9d9d0ff8048aa58c
ghazal-salehi <126438691+ghazal-salehi@users.noreply.github.com>
2023-09-15 12:14:36 +1000

    QOLDEV-569 Styling elements in searchable table (#1168)
    * QOLDEV-569 Styling elements in searchable table
    
    * QOLDEV-569 searchable table improvement
    
    * QOLDEV-569 Excluding sortable table buttons from link styles
    
    * QOLDEV-569 Excluding sortable table buttons from link styles
    
    * QOLDEV-569 typo
    
    * QOLDEV-569 Improving datatables js
    
    * QOLDEV-569 Hover state for pagination buttons
    
    * QOLDEV-569 Hover state for pagination buttons, important

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,3 +1,5 @@
-$(window).on('load', function() {
-  $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
-});
+var addQGButtonClass = function() {
+    $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
+};
+$(window).on('load', addQGButtonClass);
+$(document).on('change', '.sortable-table select', addQGButtonClass);
