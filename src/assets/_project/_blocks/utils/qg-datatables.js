34c2285423ed9e8d049e1eafd24f2deb90420331
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-20 13:22:52 +1000

    QOLDEV-569 Updating JS to make it work on FireFox

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,7 +1,6 @@
 var addQGButtonClass = function () {
   $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
 };
-//$(window).on('load', addQGButtonClass);
 window.onpageshow = function () {
   addQGButtonClass();
 };
