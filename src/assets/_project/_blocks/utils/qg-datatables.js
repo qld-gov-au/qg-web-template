90100fffe19a04122adec86075337079b4af4a2b
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-09-20 13:10:30 +1000

    onpageshow

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,5 +1,8 @@
 var addQGButtonClass = function () {
   $('.sortable-table .dataTables_wrapper a.paginate_button').addClass('qg-btn');
 };
-$(window).on('load', addQGButtonClass);
+//$(window).on('load', addQGButtonClass);
+window.onpageshow = function () {
+  addQGButtonClass();
+};
 $(document).on('change', '.sortable-table select', addQGButtonClass);
