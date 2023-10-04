9e3071dbcff60da7a2fd231d347761e92af6d72d
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-10-04 15:29:58 +1000

    QOLDEV-569 Missing trailing comma

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -2,7 +2,7 @@ if ($("table[class*='dataTable']").length > 0) {
   $('.dataTable').DataTable({
     'drawCallback': function() {
       $('.dataTables_wrapper a.paginate_button').addClass('qg-btn');
-    }
+    },
   });
 }
 
