e2a33ec9b2c2fc226e2b004a8837b75ed4d58bec
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-10-04 15:19:17 +1000

    QOLDEV-569 Using drawCallback when initialising the dataTable

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,13 +1,8 @@
 if ($("table[class*='dataTable']").length > 0) {
-  $('.dataTable').DataTable();
+  $('.dataTable').DataTable({
+    "drawCallback": function() {
+      $('.dataTables_wrapper a.paginate_button').addClass('qg-btn');
+    }
+  });
 }
 
-var addQGButtonClass = function () {
-  $('.dataTables_wrapper a.paginate_button').addClass('qg-btn');
-};
-window.onpageshow = function () {
-  addQGButtonClass();
-};
-$(document).on('change', '.dataTables_wrapper select', addQGButtonClass);
-$(document).on('change', '.dataTables_wrapper input', addQGButtonClass);
-$('.dataTable').on( 'order.dt',  addQGButtonClass);
