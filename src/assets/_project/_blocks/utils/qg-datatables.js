b1bf17b9f182d31c02b659734d2e8a5b368d9557
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2023-10-04 16:20:16 +1000

    QOLDEV-569 Other script can go into Squiz

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,14 +1,10 @@
-var addQGButtonClass = function() { $('.dataTables_wrapper a.paginate_button').addClass('qg-btn'); };
+var addQGButtonClass = function() {
+  $('.dataTables_wrapper a.paginate_button').addClass('qg-btn');
+};
+
 if ($("table[class*='dataTable']").length > 0) {
   $('.dataTable').DataTable({
     'drawCallback': addQGButtonClass,
   });
 }
 
-window.onpageshow = function() {
-  addQGButtonClass();
-};
-$(document).on('change', '.dataTables_wrapper select', addQGButtonClass);
-$(document).on('keyup', '.dataTables_wrapper input', addQGButtonClass);
-$('.dataTable').on('order.dt', addQGButtonClass);
-document.addEventListener('DOMContentLoaded', addQGButtonClass);
