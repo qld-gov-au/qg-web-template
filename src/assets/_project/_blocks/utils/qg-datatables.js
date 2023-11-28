2e6d43612cb0d06745ec959bbe87bed28041ba99
ghazal-salehi <126438691+ghazal-salehi@users.noreply.github.com>
2023-11-28 14:11:13 +1000

    Qoldev 569 searchable table resolving warning (#1326)
    * QOLDEV-569 Searchable table, resolving warning
    
    * QOLDEV-569 Initiate only when the table is not initiated already
    
    * QOLDEV-569 For each loop
    
    * QOLDEV-569 On load
    
    * QOLDEV-569 On load revert
    
    * QOLDEV-569 Styling ellipsis for dataTable pagination

--- src/assets/_project/_blocks/utils/qg-datatables.js
+++ src/assets/_project/_blocks/utils/qg-datatables.js
@@ -1,11 +1,11 @@
-var addQGButtonClass = function() {
+var addQGButtonClass = function () {
   $('.dataTables_wrapper a.paginate_button').addClass('qg-btn');
 };
 
-if ($("table[class*='dataTable']").length > 0) {
-  $('.dataTable').DataTable({
-    'drawCallback': addQGButtonClass,
-  });
-}
-
-window.onpageshow = addQGButtonClass;
+$('.dataTable').each(function () {
+  if (!$.fn.DataTable.isDataTable(this)) {
+    $(this).DataTable({
+      'drawCallback': addQGButtonClass,
+    });
+  }
+});
