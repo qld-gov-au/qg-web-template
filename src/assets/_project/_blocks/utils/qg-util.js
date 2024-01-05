59be2085c3657d04878c14f2af98219057680e97
ghazal-salehi <ghazal.salehi@smartservice.qld.gov.au>
2024-01-05 12:16:19 +1000

    Removing qg-load-google-api.js

--- src/assets/_project/_blocks/utils/qg-util.js
+++ src/assets/_project/_blocks/utils/qg-util.js
@@ -1,6 +1,6 @@
 import './qg-misc';
 import './qg-ajax-call';
-import './qg-load-google-api';
+//import './qg-load-google-api';
 import './parent-width';
 import './qg-datepicker';
 import './qg-quickexit';
